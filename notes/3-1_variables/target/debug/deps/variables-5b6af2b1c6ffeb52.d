e589aed7b8e02ea5b12ca837d779e4a9af323168
Hans Escobar <60953384+hdescobarh@users.noreply.github.com>
2022-05-10 01:03:38 -0500

    Delete notes/3-1_variables/target directory
    Removed target folder from the chapter 3-1

--- notes/3-1_variables/target/debug/deps/variables-5b6af2b1c6ffeb52.d
+++
@@ -1,5 +0,0 @@
-f:\X_Hippocampus\01_activos\rustaceology-logbook\notes\3-1_variables\target\debug\deps\variables-5b6af2b1c6ffeb52.rmeta: src\main.rs
-
-f:\X_Hippocampus\01_activos\rustaceology-logbook\notes\3-1_variables\target\debug\deps\variables-5b6af2b1c6ffeb52.d: src\main.rs
-
-src\main.rs:
--- notes/3-1_variables/target/debug/variables.d
+++
@@ -1,1 +0,0 @@
-F:\X_Hippocampus\01_activos\rustaceology-logbook\notes\3-1_variables\target\debug\variables.exe: F:\X_Hippocampus\01_activos\rustaceology-logbook\notes\3-1_variables\src\main.rs
