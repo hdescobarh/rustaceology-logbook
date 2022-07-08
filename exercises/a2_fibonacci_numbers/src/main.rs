237eba0bb17c2bbf6a71f2343412b3b587bab514
hdescobarh <escobar.hans@gmail.com>
2022-07-08 01:14:00 -0500

    Improved the code. Now checks if there is memory overflow for the iterative algorithm. If the size is bigger than 42 doesn't run the recursive algorithm. Minor improvement in memory use with mem::replace.

--- exercises/a2_fibonacci_numbers/src/main.rs
+++ exercises/a2_fibonacci_numbers/src/main.rs
@@ -1,51 +1,59 @@
 use std::io;
+use std::mem::replace;
 
-fn input_position() -> u32 {
-    println!("Which nth number do you want to find?:");
+fn input_position() -> usize {
+    println!("Which ith fibonacci number do you want to find?:");
 
     let mut input: String = String::new();
 
     io::stdin()
         .read_line(&mut input)
-        .expect("Failed to read the line");
+        .expect("Failed to read the line.\n");
 
-    input.trim().parse().expect("This is not a number")
+    input
+        .trim()
+        .parse::<usize>()
+        .expect("This is not a number.\n")
 }
 
-fn raw_fibonacci(position: u32) -> u32 {
-    let mut counter = 0;
-    let mut current_a: u32 = 0;
-    let mut current_b: u32 = 1;
+fn fibonacci_iterative(ith_fibonacci: usize) -> Option<u128> {
+    let mut current_fibo_value: u128 = 0;
+    let mut later_fibo_value: u128 = 1;
+    let mut buffer: u128;
 
-    if position == 0 {
-        return 0;
+    for _i in 0..ith_fibonacci {
+        buffer = replace(&mut later_fibo_value, current_fibo_value);
+        let check_overflow = current_fibo_value.overflowing_add(buffer);
+        if check_overflow.1 {
+            return None;
+        } else {
+            current_fibo_value = check_overflow.0;
+        }
     }
 
-    while counter < position {
-        let intermediate = current_a;
-        current_a = current_b;
-        current_b = current_b + intermediate;
-        counter = counter + 1;
-    }
-
-    return current_a;
+    Some(current_fibo_value)
 }
 
-fn naive_recursive_fibonacci(position: u32) -> u32 {
-    let base_case: [u32; 2] = [0, 1];
-
-    if base_case.contains(&position) {
-        return position;
-    } else {
-        return naive_recursive_fibonacci(position - 1) + naive_recursive_fibonacci(position - 2);
+fn fibonacci_recursive(ith_fibonacci: usize) -> u128 {
+    if ith_fibonacci <= 1 {
+        return u128::try_from(ith_fibonacci).unwrap();
     }
+
+    fibonacci_recursive(ith_fibonacci - 2) + fibonacci_recursive(ith_fibonacci - 1)
 }
 
 fn main() {
     let position = input_position();
 
-    let result_raw = raw_fibonacci(position);
-    println!("Direct fibo: F_{}= {}", position, result_raw);
-    let result_recursive = naive_recursive_fibonacci(position);
-    println!("Naive recursive: F_{}= {}", position, result_recursive);
+    let result_raw = fibonacci_iterative(position);
+    match result_raw {
+        None => println!("Overflow. Value bigger than {}", u128::MAX),
+        Some(value) => println!("Iterative: F_'{{'{}'}}'= {}", position, value),
+    }
+
+    // To avoid the long running time of recursive algorithm
+    if position <= 42 {
+        let result_recursive = fibonacci_recursive(position);
+        println!("Recursive: F_'{{'{}'}}'= {}", position, result_recursive);
+    }
 }
