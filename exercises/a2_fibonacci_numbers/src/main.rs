a9c1bee9740279df7c8831cf600f74a800a941ad
hdescobarh <escobar.hans@gmail.com>
2022-07-08 12:29:13 -0500

    Now the code can do recursion with memoization with secure addition

--- exercises/a2_fibonacci_numbers/src/main.rs
+++ exercises/a2_fibonacci_numbers/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::io;
 use std::mem::replace;
 
@@ -42,18 +43,55 @@ fn fibonacci_recursive(ith_fibonacci: usize) -> u128 {
     fibonacci_recursive(ith_fibonacci - 2) + fibonacci_recursive(ith_fibonacci - 1)
 }
 
+fn fibonacci_recursive_with_memoization(ith_fibonacci: usize) -> Option<u128> {
+    let mut memory: HashMap<usize, u128> = HashMap::from([(0, 0), (1, 1)]);
+
+    fn inner_recursion<'a>(
+        ith_fibonacci: usize,
+        memory: &'a mut HashMap<usize, u128>,
+    ) -> Option<&'a u128> {
+        if memory.contains_key(&ith_fibonacci) {
+            return memory.get(&ith_fibonacci);
+        }
+
+        let buffer1 = inner_recursion(ith_fibonacci - 1, memory).unwrap().clone();
+        let buffer2 = inner_recursion(ith_fibonacci - 2, memory).unwrap().clone();
+        memory.insert(ith_fibonacci, buffer1 + buffer2);
+        memory.get(&ith_fibonacci)
+    }
+
+    let result = inner_recursion(ith_fibonacci, &mut memory).unwrap();
+
+    if result < &u128::MAX {
+        memory.remove(&ith_fibonacci)
+    } else {
+        None
+    }
+}
+
 fn main() {
     let position = input_position();
 
     let result_raw = fibonacci_iterative(position);
     match result_raw {
-        None => println!("Overflow. Value bigger than {}", u128::MAX),
-        Some(value) => println!("Iterative: F_'{{'{}'}}'= {}", position, value),
+        None => println!("🚫 Overflow. Value bigger than {}", u128::MAX),
+        Some(value) => println!("👌 Iterative: F_'{{'{}'}}'= {}", position, value),
     }
 
     // To avoid the long running time of recursive algorithm
     if position <= 42 {
         let result_recursive = fibonacci_recursive(position);
-        println!("Recursive: F_'{{'{}'}}'= {}", position, result_recursive);
+        println!("👌 Recursive: F_'{{'{}'}}'= {}", position, result_recursive);
+    } else {
+        println!("🚫 {} ith is too long for simple recursive", position)
+    }
+
+    let result_memoization = fibonacci_recursive_with_memoization(position);
+    match result_memoization {
+        None => println!("🚫 Overflow. Value bigger than {}", u128::MAX),
+        Some(value) => println!(
+            "👌 Recursive with memoization: F_'{{'{}'}}'= {}",
+            position, value
+        ),
     }
 }
