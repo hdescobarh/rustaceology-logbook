c38eadd1d65eebdfec36134761062640c23d1a85
hdescobarh <escobar.hans@gmail.com>
2022-07-28 17:42:25 -0500

    Add comments to "4-3 slice reference" and update md file

--- notes/4-3_slice_reference/src/main.rs
+++ notes/4-3_slice_reference/src/main.rs
@@ -1,10 +1,11 @@
 fn main() {
     //slices are reference to a part of a COLLECTION
     let a_string = String::from("Hola mundo!!! :)");
+    println!("The original string is: '{}'", a_string);
     // A string slice stores on the stack a pointer to the index of the String where
     // the slice starts and the length of the slice
     let slice = string_slice_from(&a_string);
-    println!("{}", slice)
+    println!("The slice taking index 0 to 4 is: '{}'", slice);
 }
 
 fn string_slice_from(a_string: &String) -> &str {
