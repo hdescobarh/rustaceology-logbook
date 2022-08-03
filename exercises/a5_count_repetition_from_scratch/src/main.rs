9d99b83656423ee761cc9a8c5ed970b85d6fd8b6
hdescobarh <escobar.hans@gmail.com>
2022-08-02 19:29:19 -0500

    Implemented a count with HashMap.

--- exercises/a5_count_repetition_from_scratch/src/main.rs
+++ exercises/a5_count_repetition_from_scratch/src/main.rs
@@ -1,15 +1,36 @@
+use std::collections::HashMap;
 fn main() {
-    let input_string = String::from("Muchos años después, frente al pelotón...");
-    let output_count = count_characters(&input_string);
+    // case sensitive - variant sensitive (e.g. half-width ｳ vs full width ウ; Ｄ vs D)
 
-    println!("Input:\n{}\nCount:\n{:?}\n", input_string, output_count)
+    let input_string =
+        String::from("Muchos años ｳウｄdespués,  frente al pelotón...字典¡!사전¿?👾辞書");
+    let output_count = count_characters_scratch(&input_string);
+    let output_count_2 = count_character(&input_string);
+
+    println!(
+        "Input:\n{}\nCount from scratch:\n{:?}\nCount 2:\n{:?}\n",
+        input_string, output_count, output_count_2
+    )
 }
 
-// case sensitive
-fn count_characters(string: &String) -> Vec<(char, u128)> {
+fn count_character(string: &String) -> HashMap<char, usize> {
+    let mut unique_char: Vec<char> = string.chars().collect();
+    unique_char.sort();
+    unique_char.dedup(); // sort + dup -> remove all duplicates
+    let mut count_store: HashMap<char, usize> = HashMap::new();
+
+    for character in unique_char {
+        count_store.insert(character, string.matches(character).count());
+    }
+
+    count_store
+}
+
+// Using only tools of prelude
+fn count_characters_scratch(string: &String) -> Vec<(char, usize)> {
     let mut char_values: Vec<char> = Vec::new();
-    let mut count_values: Vec<u128> = Vec::new();
-    let mut result: Vec<(char, u128)> = Vec::new();
+    let mut count_values: Vec<usize> = Vec::new();
+    let mut result: Vec<(char, usize)> = Vec::new();
 
     for character in string.chars() {
         // if char in char_values then add one more to the same index in count_values
