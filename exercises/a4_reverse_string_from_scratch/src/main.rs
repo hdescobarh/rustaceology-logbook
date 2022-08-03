57a3baf20bd02ffd11dd97eaa4cf041e7f686f0b
hdescobarh <escobar.hans@gmail.com>
2022-08-02 19:19:28 -0500

    Added a demo. Now also now can uses a method using all standard library tools

--- exercises/a4_reverse_string_from_scratch/src/main.rs
+++ exercises/a4_reverse_string_from_scratch/src/main.rs
@@ -1,12 +1,28 @@
 use std::io;
 
 fn main() {
+    // Example
+    demo();
+    // Input
     let original_string = read_input();
-    let reverse_string = reverse_from_string(&original_string);
+    let reverse_easy = invert_from_string_easy(&original_string);
+    let reverse_string_scratch = reverse_from_string_scratch(&original_string);
     println!(
-        "Original:\n{}\nReverse:\n{}",
-        original_string, reverse_string
+        "Original:\n{}\nReverse simple:\n{}\nReverse implemented from scratch:\n{}\n",
+        original_string, reverse_easy, reverse_string_scratch
     );
+}
+
+fn demo() {
+    println!("Demo start:\n");
+    let demo = String::from("Muchos años después, frente al pelotón...字典¡!사전¿?👾辞書");
+    let reverted_input = invert_from_string_easy(&demo);
+    let reverted_scratch = reverse_from_string_scratch(&demo);
+    println!(
+        "Original:\n{}\nReverse simple:\n{}\nReverse implemented from scratch:\n{}\n",
+        demo, reverted_input, reverted_scratch
+    );
+    println!("Demo end:\n");
 }
 
 fn read_input() -> String {
@@ -18,15 +34,21 @@ fn read_input() -> String {
     input.trim().to_string()
 }
 
-fn reverse_from_string(original_string: &String) -> String {
+fn invert_from_string_easy(original: &String) -> String {
+    let char_iterator = original.chars().rev();
+    String::from_iter(char_iterator)
+}
+
+fn reverse_from_string_scratch(original_string: &String) -> String {
     let lenght = original_string.len();
-    let mut char_vector: Vec<char> = vec!['\0'; lenght];
+    let mut char_vector: Vec<char> = vec!['\0'; lenght]; //Generates error if try to acces index i when the actual lenght is lower than i
     let mut reverse = String::with_capacity(lenght);
-    let mut char_iterator = original_string.chars();
+    let mut char_iterator = original_string.chars(); // Using method nth(i) will remove i
+    let max_index = lenght - 1;
 
     for index in 0..lenght {
         if let Some(character) = char_iterator.next() {
-            char_vector[lenght - 1 - index] = character;
+            char_vector[max_index - index] = character;
         };
     }
 
