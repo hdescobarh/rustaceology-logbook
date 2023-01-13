37e5698568f0e84f4a331b2330c6d6e1e948caba
Hans Escobar <hdescobarh@unal.edu.co>
2023-01-12 22:40:46 -0500

    Finished wasm + webpack excercise

--- exercises/w2_rust-wasm-app/rust_wasm_dev/src/lib.rs
+++ exercises/w2_rust-wasm-app/rust_wasm_dev/src/lib.rs
@@ -2,8 +2,9 @@ mod utils;
 
 use wasm_bindgen::prelude::*;
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#[wasm_bindgen]
+pub fn add_unumbers(left: usize, right: usize) -> Option<usize> {
+    left.checked_add(right)
 }
 
 #[wasm_bindgen]
@@ -12,6 +13,61 @@ extern "C" {
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, world!");
+pub fn greet(message: &str) {
+    alert(&format!("Hello, {}!", message));
+}
+
+// Using str and String requieres to perform copies between the JavaScript garbage-collected heap and the Wasm linear memory
+#[wasm_bindgen]
+pub fn text_lenght_a(text: &str) -> usize {
+    text.char_indices().count()
+}
+
+// js_sys crate allows to access directly to JavaScript memory
+#[wasm_bindgen]
+pub fn text_lenght_b(text: &js_sys::JsString) -> u32 {
+    text.length()
+}
+
+#[wasm_bindgen]
+pub struct Baz {
+    field0: u32,
+    field1: i32,
+    pub field2: u32,
+    #[wasm_bindgen(readonly)]
+    pub field3: u32,
+}
+
+// const obj = new Baz(3);
+// assert.equal(obj.field, 3);
+// obj.field = 4;
+// assert.equal(obj.field, 4);
+// console.log(obj.get_contents());
+#[wasm_bindgen]
+impl Baz {
+    #[wasm_bindgen(constructor)]
+    pub fn new(field1: i32) -> Baz {
+        Baz {
+            field1,
+            field2: 50u32,
+            field3: 10u32,
+            field0: 15u32,
+        }
+    }
+
+    // The default name for a setter is the function's name minus the set_ prefix, and if set_ isn't a prefix of the function it's an error to not provide the name explicitly.
+    #[wasm_bindgen(setter)]
+    pub fn set_field1(&mut self, field: i32) {
+        self.field1 = field;
+    }
+
+    // The name for a getter is by default inferred from the function name it's attached to
+    #[wasm_bindgen(getter)]
+    pub fn field1(&self) -> i32 {
+        self.field1
+    }
+
+    pub fn get_field1(&self) -> i32 {
+        self.field1
+    }
 }
--- exercises/w2_rust-wasm-app/source/index.js
+++ exercises/w2_rust-wasm-app/source/index.js
@@ -1,3 +1,39 @@
 import * as wasm from "rust-wasm-app-template";
 
-wasm.greet("WebAssembly with npm");
+// Rust defined function using JS greet function
+wasm.greet("Hansu")
+// Rust exported function
+let sum = wasm.add_unumbers(40, 2);
+document.getElementById("test_value").innerHTML = "Answer to the Ultimate Question of Life,\nthe Universe,\n and Everything:\n" + sum.toString();
+
+const obj = new wasm.Baz(3);
+// get by custom function
+console.log("get an private field by a custom function\n field1: ", obj.get_field1())
+// set a private field
+console.log("set an exposed private field\n field1=30")
+obj.field1 = 30;
+// get from a private field
+console.log("get from an exposed private field\n field1: ", obj.field1)
+// set a public field
+console.log("set the public field to 100\n field2=100")
+obj.field2 = 100
+// get a public field
+console.log("get the previous public field\n field2: ", obj.field2)
+// get an only-read pub field
+console.log("Get and only-read public field\n field3: ", obj.field3)
+// try to set an only-read pub field
+console.log("Try to set an only-read public field:\n")
+try { obj.field3 = 65 } catch (e) {
+    console.error(e)
+};
+// trying to read a private field
+console.log("trying to read a private field\n field0: ", obj.field0)
+console.assert(15 == obj.field0, "Unaccesible")
+// trying to set a private field
+console.log("Be aware of this behaviour:\n")
+console.log("trying to set a private field\n field0=1000")
+try { obj.field0 = 1000; } catch (e) {
+    console.error(e)
+};
+console.log("trying to read a private field\n field0: ", obj.field0)
+console.log("Now the JS object field0 attribute points to something different to the Rust structure instance field0")
