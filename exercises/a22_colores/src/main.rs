4d3ce448fdb9e2d8688ab56a5255a0cb218d9b47
Hans D. Escobar H <hdescobarh@unal.edu.co>
2023-09-18 17:04:06 -0500

    chore(a22): agregada la descripción del problema

--- exercises/a22_colores/src/main.rs
+++ exercises/a22_colores/src/main.rs
@@ -1,3 +1,13 @@
+// author: Hans D. Escobar H. (hdescobarh)
+
+/*
+Crea las funciones capaces de transformar colores HEX
+a RGB y viceversa.
+Ejemplos:
+RGB a HEX: r: 0, g: 0, b: 0 -> #000000
+HEX a RGB: hex: #000000 -> (r: 0, g: 0, b: 0)
+*/
+
 fn main() {
     println!("Hello, world!");
 }
