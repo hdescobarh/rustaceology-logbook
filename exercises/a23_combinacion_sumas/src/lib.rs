f84e14d640ae4a679770119a922a74d0ec910435
Hans D. Escobar H <hdescobarh@unal.edu.co>
2023-09-25 18:23:34 -0500

    chore(a23): inited excersice

---
+++ exercises/a23_combinacion_sumas/src/lib.rs
@@ -0,0 +1,29 @@
+// author: Hans D. Escobar H. (hdescobarh)
+
+/*
+ * Crea una función que encuentre todas las combinaciones de los números
+ * de una lista que suman el valor objetivo.
+ * - La función recibirá una lista de números enteros positivos
+ *   y un valor objetivo.
+ * - Para obtener las combinaciones sólo se puede usar
+ *   una vez cada elemento de la lista (pero pueden existir
+ *   elementos repetidos en ella).
+ * - Ejemplo: Lista = [1, 5, 3, 2],  Objetivo = 6
+ *   Soluciones: [1, 5] y [1, 3, 2] (ambas combinaciones suman 6)
+ *   (Si no existen combinaciones, retornar una lista vacía)
+ */
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
