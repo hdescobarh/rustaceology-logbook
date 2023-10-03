88a0cc99bff77d278b3f8a02e2f196b2c1215436
Hans D. Escobar H <hdescobarh@unal.edu.co>
2023-10-02 19:49:44 -0500

    chore(a24): agregada descripción del ejercicio

---
+++ exercises/a24_triples_pitagoricos/src/lib.rs
@@ -0,0 +1,19 @@
+// author: Hans D. Escobar H. (hdescobarh)
+#![crate_name = "triples_pitagoricos"]
+#![crate_type = "cdylib"]
+
+/*
+Reto #39: Triples pitagóricos
+ Crea una función que encuentre todos los triples pitagóricos
+ (ternas) menores o iguales a un número dado.
+ - Debes buscar información sobre qué es un triple pitagórico.
+ - La función únicamente recibe el número máximo que puede
+   aparecer en el triple.
+ - Ejemplo: Los triples menores o iguales a 10 están
+   formados por (3, 4, 5) y (6, 8, 10).
+ */
+
+#[cfg(test)]
+mod tests {
+    //use super::*;
+}
