e7609de35dcec3966f463105affbb6f2584c9709
Hans D. Escobar H <hdescobarh@unal.edu.co>
2023-10-02 22:25:27 -0500

    feat(a24): ahora encuentra todos los divisores de un número)

--- exercises/a24_triples_pitagoricos/src/lib.rs
+++ exercises/a24_triples_pitagoricos/src/lib.rs
@@ -24,39 +24,56 @@ Reto #39: Triples pitagóricos
 Mi estrategia consiste en aplicar (1) para encontrar los dos valores restantes que satisfacen el triple;
 posteriormente, a través de los divisores comunes (2) encontrar los triples menores asociados.
 */
-type Entero = u64;
 
 pub struct TriplePitagorico {
-    cateto_a: Entero,
-    cateto_b: Entero,
-    hipotenusa: Entero,
+    cateto_a: usize,
+    cateto_b: usize,
+    hipotenusa: usize,
 }
 
 impl TriplePitagorico {
     // crea un Tripe Pitagórico a partir de un numero, el cual se asume que es el máximo del trio y , por lo tanto
     // la hipotenusa
-    pub fn desde_numero_maximo(numero: &Entero) {}
+    pub fn desde_numero_maximo(numero: &usize) {}
 
     // devuelve una lista de los divisores comunes del triple diferentes de 1
     // si el máximo común divisor es uno, retorna None
-    fn encontrar_divisores_comunes(&self) {}
+    fn encontrar_divisores_comunes(&self) {
+        // primero encuentra el máximo común divisor para los tres números. Se aplica la propiedad:
+        // gcd(a, b, c) = gcd(a, gcd(b, c)) = gcd(gcd(a, b), c) = gcd(gcd(a, c), b).
+        let triple_mcd = maximo_comun_divisor(
+            &self.hipotenusa,
+            &maximo_comun_divisor(&self.cateto_a, &self.cateto_b),
+        );
+        // seguido, se encuentra todos los divisores del mcd.
+        // Estos serán todos los divisores posibles de triple
+    }
 
     //
     fn generar_triples_menores(&self) {}
 }
 
-// gcd(a, b, c) = gcd(a, gcd(b, c)) = gcd(gcd(a, b), c) = gcd(gcd(a, c), b).
-
+// encuentra el máximo común divisor de un par de números enteros positivos
 // implementación naive del algoritmo de Euclides
-fn maximo_comun_divisor_dupla(numero_1: &Entero, numero_2: &Entero) -> Entero {
+fn maximo_comun_divisor(numero_1: &usize, numero_2: &usize) -> usize {
     match numero_1.cmp(&numero_2) {
         Ordering::Equal => *numero_1,
-        Ordering::Greater => maximo_comun_divisor_dupla(&(numero_1 - numero_2), numero_2),
-        Ordering::Less => maximo_comun_divisor_dupla(numero_1, &(numero_2 - numero_1)),
+        Ordering::Greater => maximo_comun_divisor(&(numero_1 - numero_2), numero_2),
+        Ordering::Less => maximo_comun_divisor(numero_1, &(numero_2 - numero_1)),
     }
 }
 
-fn divisores_entero_positivo() {}
+// encuentra todos los divisores de un número entero positivo
+// implementación naive
+fn obtener_todos_los_divisores(numero: &usize) -> Vec<usize> {
+    let mut divisores: Vec<usize> = Vec::with_capacity(*numero);
+    for candidato in 1..(numero + 1) {
+        if (numero % candidato) == 0 {
+            divisores.push(candidato)
+        }
+    }
+    divisores
+}
 
 #[cfg(test)]
 mod tests {
@@ -64,10 +81,24 @@ mod tests {
 
     #[test]
     fn calcula_correctamente_mcd() {
-        let test_cases: [[Entero; 3]; 4] =
+        let test_cases: [[usize; 3]; 4] =
             [[36, 60, 12], [154, 374, 22], [2377, 1284, 1], [57, 87, 3]];
         for case in test_cases {
-            assert_eq!(case[2], maximo_comun_divisor_dupla(&case[0], &case[1]))
+            assert_eq!(case[2], maximo_comun_divisor(&case[0], &case[1]))
+        }
+    }
+
+    #[test]
+    fn encuentra_los_divisores_de_un_numero() {
+        let test_cases = [
+            (7, vec![1, 7]),
+            (12, vec![1, 2, 3, 4, 6, 12]),
+            (25, vec![1, 5, 25]),
+            (42, vec![1, 2, 3, 6, 7, 14, 21, 42]),
+        ];
+
+        for case in test_cases {
+            assert_eq!(case.1, obtener_todos_los_divisores(&case.0));
         }
     }
 }
