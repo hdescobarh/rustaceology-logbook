6f943813935e9ed533bf0d487d647b48b30968c2
Hans D. Escobar H <hdescobarh@unal.edu.co>
2023-11-20 18:22:14 -0500

    chore(a29): init challenge

---
+++ exercises/a29_crud_sorteo/src/main.rs
@@ -0,0 +1,16 @@
+// author: Hans D. Escobar H. (hdescobarh)
+
+/* Reto #45: El calendario de aDEViento 2023
+Crea un programa que simule el mecanismo de participación de aDEViento:
+- Mediante la terminal, el programa te preguntará si quieres añadir y borrar
+participantes, mostrarlos, lanzar el sorteo o salir.
+- Si seleccionas añadir un participante, podrás escribir su nombre y pulsar enter.
+- Si seleccionas añadir un participante, y este ya existe, avisarás de ello. (Y no lo duplicarás)
+- Si seleccionas mostrar los participantes, se listarán todos.
+- Si seleccionas eliminar un participante, podrás escribir su nombre y
+pulsar enter. (Avisando de si lo has eliminado o el nombre no existe)
+- Si seleccionas realizar el sorteo, elegirás una persona al azar y se eliminará del listado.
+- Si seleccionas salir, el programa finalizará.
+ */
+
+fn main() {}
