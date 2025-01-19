63917ce36bbb0b60c84dcae4445edd0e917bf9df
Hans Escobar <hans-escobar@proton.me>
2025-01-19 14:18:39 -0500

    refactor: now uses windows method

--- exercises/_exercism/6_sublist/src/lib.rs
+++ exercises/_exercism/6_sublist/src/lib.rs
@@ -24,31 +24,13 @@ fn is_sublist<T: PartialEq>(first_list: &[T], second_list: &[T]) -> bool {
         return false;
     }
 
-    // Empty list case
     if first_list.is_empty() && !second_list.is_empty() {
-        return true;
+        true
+    } else {
+        second_list
+            .windows(first_list.len())
+            .any(|window| window == first_list)
     }
-
-    // Non-empty list case
-    let free_spaces = second_list.len() - first_list.len();
-    let mut index = 0;
-
-    while index <= free_spaces {
-        if let Some(position) = second_list[index..]
-            .iter()
-            .position(|value| *value == first_list[index])
-        {
-            if second_list[position..position + first_list.len()] == *first_list {
-                return true;
-            } else {
-                index = position + 1;
-                continue;
-            }
-        }
-        return false;
-    }
-
-    false
 }
 
 fn is_superlist<T: PartialEq>(first_list: &[T], second_list: &[T]) -> bool {
@@ -59,27 +41,10 @@ fn is_superlist<T: PartialEq>(first_list: &[T], second_list: &[T]) -> bool {
 
     // Empty list case
     if !first_list.is_empty() && second_list.is_empty() {
-        return true;
+        true
+    } else {
+        first_list
+            .windows(second_list.len())
+            .any(|window| window == second_list)
     }
-
-    // Non-empty list case
-    let free_spaces = first_list.len() - second_list.len();
-    let mut index = 0;
-
-    while index <= free_spaces {
-        if let Some(position) = first_list[index..]
-            .iter()
-            .position(|value| *value == second_list[index])
-        {
-            if first_list[position..position + second_list.len()] == *second_list {
-                return true;
-            } else {
-                index = position + 1;
-                continue;
-            }
-        }
-        return false;
-    }
-
-    false
 }
