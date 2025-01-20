5c20b1cb5bebf94477d954c57884d7c765fb0b3c
Hans Escobar <hans-escobar@proton.me>
2025-01-20 17:56:22 -0500

    refactor: removed unnecesary collect

--- exercises/_exercism/9_armstrong-numbers/src/lib.rs
+++ exercises/_exercism/9_armstrong-numbers/src/lib.rs
@@ -1,10 +1,8 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    let digits: Vec<u32> = num
-        .to_string()
+    let num_str = num.to_string();
+    let exponent: u32 = num_str.len().try_into().unwrap();
+    num == num_str
         .chars()
         .map(|c| c.to_digit(10).unwrap())
-        .collect();
-
-    let exponent: u32 = digits.len().try_into().unwrap();
-    digits.into_iter().fold(0, |acc, v| v.pow(exponent) + acc) == num
+        .fold(0, |acc, value: u32| value.pow(exponent) + acc)
 }
