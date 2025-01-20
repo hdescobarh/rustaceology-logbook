336bd824d420c1309fef1584f3ee7bf7f2286884
Hans Escobar <hans-escobar@proton.me>
2025-01-20 12:26:28 -0500

    feat: solved luhn challenge

--- exercises/_exercism/8_luhn/src/lib.rs
+++ exercises/_exercism/8_luhn/src/lib.rs
@@ -1,4 +1,33 @@
 /// Check a Luhn checksum.
 pub fn is_valid(code: &str) -> bool {
-    todo!("Is the Luhn checksum for {code} valid?");
+    let mut valid_digits = 0_u32;
+    let mut sum = 0_u32;
+
+    for c in code.chars().rev() {
+        if c.is_ascii_whitespace() {
+            continue;
+        }
+
+        match c.to_digit(10) {
+            Some(value) => {
+                if valid_digits.rem_euclid(2) == 0 {
+                    sum += value;
+                } else {
+                    sum += double_and_lower_than_nine(value)
+                }
+                valid_digits += 1
+            }
+            None => return false,
+        }
+    }
+
+    valid_digits > 1 && sum.rem_euclid(10) == 0
+}
+
+fn double_and_lower_than_nine(value: u32) -> u32 {
+    if value < 5 {
+        value * 2
+    } else {
+        value * 2 - 9
+    }
 }
