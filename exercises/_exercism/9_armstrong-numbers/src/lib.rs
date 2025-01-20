f86117649fe3f08c02480c04632d5681c41ee60e
Hans Escobar <hans-escobar@proton.me>
2025-01-20 17:43:39 -0500

    feat: solved exercise

--- exercises/_exercism/9_armstrong-numbers/src/lib.rs
+++ exercises/_exercism/9_armstrong-numbers/src/lib.rs
@@ -1,3 +1,10 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    todo!("true if {num} is an armstrong number")
+    let digits: Vec<u32> = num
+        .to_string()
+        .chars()
+        .map(|c| c.to_digit(10).unwrap())
+        .collect();
+
+    let exponent: u32 = digits.len().try_into().unwrap();
+    digits.into_iter().fold(0, |acc, v| v.pow(exponent) + acc) == num
 }
