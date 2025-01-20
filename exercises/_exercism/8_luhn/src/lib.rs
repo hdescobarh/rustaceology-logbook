0263f23a5fa5dbec1d5e90b916aba57545619685
Hans Escobar <hans-escobar@proton.me>
2025-01-20 12:45:37 -0500

    refactor: removed unnecesary auxiliar function, improved redability

--- exercises/_exercism/8_luhn/src/lib.rs
+++ exercises/_exercism/8_luhn/src/lib.rs
@@ -2,32 +2,21 @@
 pub fn is_valid(code: &str) -> bool {
     let mut valid_digits = 0_u32;
     let mut sum = 0_u32;
-
     for c in code.chars().rev() {
         if c.is_ascii_whitespace() {
             continue;
         }
-
-        match c.to_digit(10) {
-            Some(value) => {
+        match c.to_digit(10).and_then(|v| v.checked_mul(2)) {
+            Some(double) => {
                 if valid_digits.rem_euclid(2) == 0 {
-                    sum += value;
+                    sum += double;
                 } else {
-                    sum += double_and_lower_than_nine(value)
+                    sum += if double < 10 { double } else { double - 9 }
                 }
                 valid_digits += 1
             }
             None => return false,
         }
     }
-
-    valid_digits > 1 && sum.rem_euclid(10) == 0
-}
-
-fn double_and_lower_than_nine(value: u32) -> u32 {
-    if value < 5 {
-        value * 2
-    } else {
-        value * 2 - 9
-    }
+    valid_digits > 1 && sum.checked_rem_euclid(10).is_some_and(|s| s == 0)
 }
