57aa700bb83a781ac54284d970915dbb677cf8f1
Hans Escobar <hans-escobar@proton.me>
2025-01-19 19:49:42 -0500

    feat: added solution

--- exercises/_exercism/7_minesweeper/src/lib.rs
+++ exercises/_exercism/7_minesweeper/src/lib.rs
@@ -1,3 +1,58 @@
+const MINE_BYTE: u8 = 42;
+const EMPTY_BYTE: u8 = 32;
+const DIGIT_ONE: u8 = 49;
+
 pub fn annotate(minefield: &[&str]) -> Vec<String> {
-    todo!("\nAnnotate each square of the given minefield with the number of mines that surround said square (blank if there are no surrounding mines):\n{minefield:#?}\n");
+    let mut bytes_table: Vec<Vec<u8>> = minefield
+        .iter()
+        .map(|row| row.as_bytes().to_owned())
+        .collect();
+
+    let field_rows = bytes_table.len();
+    let field_cols = bytes_table.first().map(|row| row.len()).unwrap_or(0);
+
+    for row in 0..field_rows {
+        for col in 0..field_cols {
+            if bytes_table[row][col] != MINE_BYTE {
+                continue;
+            }
+
+            for (i, j) in get_neighborhood(row, col, field_rows, field_cols) {
+                if bytes_table[i][j] == MINE_BYTE {
+                    continue;
+                } else if bytes_table[i][j] == EMPTY_BYTE {
+                    bytes_table[i][j] = DIGIT_ONE
+                } else {
+                    bytes_table[i][j] += 1
+                }
+            }
+        }
+    }
+
+    bytes_table
+        .into_iter()
+        .map(|row| String::from_utf8(row).unwrap())
+        .collect()
+}
+
+// Gets the valid 1 square neighborhood
+fn get_neighborhood(
+    row: usize,
+    col: usize,
+    field_rows: usize,
+    field_cols: usize,
+) -> impl Iterator<Item = (usize, usize)> {
+    [
+        (-1, -1),
+        (-1, 0),
+        (-1, 1),
+        (0, -1),
+        (0, 1),
+        (1, -1),
+        (1, 0),
+        (1, 1),
+    ]
+    .iter()
+    .filter_map(move |(i, j)| Some((row.checked_add_signed(*i)?, col.checked_add_signed(*j)?)))
+    .filter(move |(i, j)| *i < field_rows && *j < field_cols)
 }
