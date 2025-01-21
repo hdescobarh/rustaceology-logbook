b33bda23e7312bfcb3bb5c37a4532f39d9583dd4
Hans Escobar <hans-escobar@proton.me>
2025-01-21 17:46:12 -0500

    feat: solved leap exercise

--- exercises/_exercism/13_leap/src/lib.rs
+++ exercises/_exercism/13_leap/src/lib.rs
@@ -1,3 +1,8 @@
 pub fn is_leap_year(year: u64) -> bool {
-    todo!("true if {year} is a leap year")
+    year.checked_rem(4).is_some_and(|v| v == 0)
+        && match year.checked_rem(100) {
+            Some(0) => year.checked_rem(400).is_some_and(|v| v == 0),
+            Some(_) => true,
+            None => false,
+        }
 }
