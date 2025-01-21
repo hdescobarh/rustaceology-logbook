cdd4318ab34a65d492bdb994156ae62487e8befa
Hans Escobar <hans-escobar@proton.me>
2025-01-21 15:53:31 -0500

    feat: challenge solved

--- exercises/_exercism/12_grains/src/lib.rs
+++ exercises/_exercism/12_grains/src/lib.rs
@@ -1,7 +1,8 @@
 pub fn square(s: u32) -> u64 {
-    todo!("grains of rice on square {s}");
+    2_u64.pow(s - 1)
 }
 
 pub fn total() -> u64 {
-    todo!();
+    // 2_u128.pow(64) - 1 also works, but requires u128
+    (1..=64).fold(0, |acc, s| square(s) + acc)
 }
