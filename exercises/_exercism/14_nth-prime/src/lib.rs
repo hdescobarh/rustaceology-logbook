e204e284dafb3e6781e6d905a76db7418406ed9a
Hans Escobar <hans-escobar@proton.me>
2025-01-22 12:56:22 -0500

    feat: now uses Rosser's (1939) formula to define a upper bound for the sieve. Rosser, B. (1939), The n-th Prime is Greater than nlogn. Proceedings of the London Mathematical Society, s2-45: 21-44. https://doi.org/10.1112/plms/s2-45.1.21

--- exercises/_exercism/14_nth-prime/src/lib.rs
+++ exercises/_exercism/14_nth-prime/src/lib.rs
@@ -1,6 +1,16 @@
 pub fn nth(n: u32) -> u32 {
-    let upper_bound = 150_000;
+    let upper_bound = get_upper_bound(n);
     sieve_eratosthenes(upper_bound).nth(n as usize + 2).unwrap() as u32
+}
+
+// p(n) < n ln(n + 2) n log log n for n > 3 (doi: 10.1112/plms/s2-45.1.21)
+fn get_upper_bound(n: u32) -> usize {
+    let k = (n + 1) as f64;
+    if n <= 3 {
+        6
+    } else {
+        (k * k.ln() + 2.0 * k * k.ln().ln()) as usize
+    }
 }
 
 fn sieve_eratosthenes(upper_bound: usize) -> impl Iterator<Item = usize> {
