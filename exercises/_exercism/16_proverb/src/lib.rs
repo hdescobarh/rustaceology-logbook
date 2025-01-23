e697d9e75e026da4dcde67986570a7d91b31375d
Hans Escobar <hans-escobar@proton.me>
2025-01-22 22:58:21 -0500

    feat: solved exercise

--- exercises/_exercism/16_proverb/src/lib.rs
+++ exercises/_exercism/16_proverb/src/lib.rs
@@ -1,3 +1,14 @@
+use std::fmt::Write;
+
 pub fn build_proverb(list: &[&str]) -> String {
-    todo!("build a proverb from this list of items: {list:?}")
+    let mut buffer = String::new();
+
+    list.windows(2).for_each(|w| {
+        writeln!(&mut buffer, "For want of a {} the {} was lost.", w[0], w[1]).unwrap()
+    });
+
+    list.first()
+        .and_then(|s| write!(&mut buffer, "And all for the want of a {s}.").ok());
+
+    buffer
 }
