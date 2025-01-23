c6b5fc660f6c5806ca8c293a97115ac33e570516
Hans Escobar <hans-escobar@proton.me>
2025-01-23 15:33:57 -0500

    feat: exercise solved

--- exercises/_exercism/18_sum-of-multiples/src/lib.rs
+++ exercises/_exercism/18_sum-of-multiples/src/lib.rs
@@ -1,3 +1,10 @@
+use std::collections::HashSet;
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    todo!("Sum the multiples of all of {factors:?} which are less than {limit}")
+    factors
+        .iter()
+        .filter(|base_value| **base_value != 0)
+        .flat_map(|base_value| (*base_value..limit).step_by(*base_value as usize))
+        .collect::<HashSet<u32>>()
+        .iter()
+        .sum()
 }
