7d330fc90df10a3b0a404e256581dab4deec7697
Hans Escobar <hans-escobar@proton.me>
2025-01-23 15:07:22 -0500

    feat: exercise solved

--- exercises/_exercism/17_raindrops/src/lib.rs
+++ exercises/_exercism/17_raindrops/src/lib.rs
@@ -1,3 +1,16 @@
+use std::fmt::Write;
+const DICT: [(u32, &str); 3] = [(3, "i"), (5, "a"), (7, "o")];
 pub fn raindrops(n: u32) -> String {
-    todo!("what sound does Raindrop #{n} make?")
+    let mut buffer = String::new();
+    DICT.iter().for_each(|(d, c)| {
+        if n % d == 0 {
+            write!(&mut buffer, "Pl{}ng", c).unwrap();
+        }
+    });
+
+    if buffer.is_empty() {
+        n.to_string()
+    } else {
+        buffer
+    }
 }
