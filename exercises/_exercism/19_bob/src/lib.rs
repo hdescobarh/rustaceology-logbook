572aea97a1922647afe5373bf87839ae38884eed
Hans Escobar <hans-escobar@proton.me>
2025-01-24 11:58:48 -0500

    feat: exercise solved

--- exercises/_exercism/19_bob/src/lib.rs
+++ exercises/_exercism/19_bob/src/lib.rs
@@ -1,3 +1,18 @@
 pub fn reply(message: &str) -> &str {
-    todo!("have Bob reply to the incoming message: {message}")
+    let message: Vec<char> = message.trim().chars().collect();
+    if message.is_empty() {
+        return "Fine. Be that way!";
+    }
+    let is_asking = message.last().is_some_and(|c| *c == '?');
+    let mut only_alphabetic = message.iter().filter(|c| c.is_alphabetic()).peekable();
+    let is_yelling = match only_alphabetic.peek() {
+        Some(_) => only_alphabetic.all(|c| c.is_uppercase()),
+        None => false,
+    };
+    match (is_asking, is_yelling) {
+        (true, true) => "Calm down, I know what I'm doing!",
+        (true, false) => "Sure.",
+        (false, true) => "Whoa, chill out!",
+        (false, false) => "Whatever.",
+    }
 }
