4eef622c3c37b7666a29853f8a51d04858ed92dd
Hans Escobar <hans-escobar@proton.me>
2025-01-27 14:09:58 -0500

    feat: exercise solved

--- exercises/_exercism/20_high-scores/src/lib.rs
+++ exercises/_exercism/20_high-scores/src/lib.rs
@@ -1,24 +1,31 @@
 #[derive(Debug)]
-pub struct HighScores;
+pub struct HighScores {
+    values: Vec<u32>,
+}
 
 impl HighScores {
     pub fn new(scores: &[u32]) -> Self {
-        todo!("Construct a HighScores struct, given the scores: {scores:?}")
+        Self {
+            values: scores.to_vec(),
+        }
     }
 
     pub fn scores(&self) -> &[u32] {
-        todo!("Return all the scores as a slice")
+        &self.values
     }
 
     pub fn latest(&self) -> Option<u32> {
-        todo!("Return the latest (last) score")
+        self.values.last().copied()
     }
 
     pub fn personal_best(&self) -> Option<u32> {
-        todo!("Return the highest score")
+        self.values.iter().max().copied()
     }
 
     pub fn personal_top_three(&self) -> Vec<u32> {
-        todo!("Return 3 highest scores")
+        let mut to_sort = self.values.clone();
+        to_sort.sort_by(|a, b| b.cmp(a));
+        to_sort.truncate(3);
+        to_sort
     }
 }
