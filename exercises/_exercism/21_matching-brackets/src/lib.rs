016d1d523b7d1654cbca33c40d9130bfc7be1490
Hans Escobar <hans-escobar@proton.me>
2025-01-27 15:10:22 -0500

    feat: solved exercise

--- exercises/_exercism/21_matching-brackets/src/lib.rs
+++ exercises/_exercism/21_matching-brackets/src/lib.rs
@@ -1,3 +1,28 @@
 pub fn brackets_are_balanced(string: &str) -> bool {
-    todo!("Check if the string \"{string}\" contains balanced brackets");
+    let mut stack: Vec<&str> = Vec::new();
+    for s in string.matches(&['{', '}', '(', ')', '[', ']']) {
+        if ["{", "(", "["].contains(&s) {
+            stack.push(s);
+            continue;
+        }
+        match stack.pop() {
+            Some(opening) => {
+                if !matching_closing(opening, s) {
+                    return false;
+                }
+            }
+            None => return false,
+        }
+    }
+
+    stack.is_empty()
+}
+
+fn matching_closing(open: &str, closing: &str) -> bool {
+    match open {
+        "{" => "}" == closing,
+        "(" => ")" == closing,
+        "[" => "]" == closing,
+        _ => false,
+    }
 }
