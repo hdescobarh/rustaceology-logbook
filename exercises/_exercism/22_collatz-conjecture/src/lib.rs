be22275a46d2ab682aca9580672e050922531523
Hans Escobar <hans-escobar@proton.me>
2025-01-27 20:30:25 -0500

    feat: exercise solved

--- exercises/_exercism/22_collatz-conjecture/src/lib.rs
+++ exercises/_exercism/22_collatz-conjecture/src/lib.rs
@@ -1,3 +1,15 @@
-pub fn collatz(n: u64) -> Option<u64> {
-    todo!("return Some(x) where x is the number of steps required to reach 1 starting with {n}")
+pub fn collatz(mut n: u64) -> Option<u64> {
+    if n == 0 {
+        return None;
+    }
+    let mut counter = 0_u64;
+    while n != 1 {
+        n = match n.checked_rem_euclid(2) {
+            Some(0) => n / 2,
+            Some(_) => n.checked_mul(3).and_then(|v| v.checked_add(1))?,
+            None => return None,
+        };
+        counter = counter.checked_add(1)?
+    }
+    Some(counter)
 }
