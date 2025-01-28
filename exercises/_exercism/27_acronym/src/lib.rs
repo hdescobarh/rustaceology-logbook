a7d4553bf2c7cc6fb1bee0bc574a81270ac2a7c9
Hans Escobar <hans-escobar@proton.me>
2025-01-28 17:54:54 -0500

    refactor: improved readibility separating complex steps in separated functions

--- exercises/_exercism/27_acronym/src/lib.rs
+++ exercises/_exercism/27_acronym/src/lib.rs
@@ -1,29 +1,34 @@
-use std::fmt::Write;
-
 pub fn abbreviate(phrase: &str) -> String {
-    let mut buffer = String::new();
-    for word in phrase.split(&['-', ' ']) {
-        let letters = if word.chars().any(|c| c.is_alphabetic() && c.is_lowercase()) {
-            word.chars()
-                .filter(|c| c.is_alphabetic())
-                .enumerate()
-                .filter_map(|(index, c)| {
-                    if index == 0 || c.is_uppercase() {
-                        Some(c)
-                    } else {
-                        None
-                    }
-                })
-                .collect::<String>()
-                .to_uppercase()
-        } else {
-            match word.chars().find(|c| c.is_uppercase()) {
-                Some(c) => c.to_string(),
-                None => "".to_string(),
+    phrase
+        .split(&['-', ' '])
+        .map(|word| {
+            if word.chars().any(|c| c.is_alphabetic() && c.is_lowercase()) {
+                initial_from_mixed(word)
+            } else {
+                intial_from_all_upper(word)
             }
-        };
+        })
+        .collect()
+}
 
-        write!(&mut buffer, "{}", letters).unwrap();
+fn initial_from_mixed(word: &str) -> String {
+    word.chars()
+        .filter(|c| c.is_alphabetic())
+        .enumerate()
+        .filter_map(|(index, c)| {
+            if index == 0 || c.is_uppercase() {
+                Some(c)
+            } else {
+                None
+            }
+        })
+        .collect::<String>()
+        .to_uppercase()
+}
+
+fn intial_from_all_upper(word: &str) -> String {
+    match word.chars().find(|c| c.is_uppercase()) {
+        Some(c) => c.to_string(),
+        None => "".to_string(),
     }
-    buffer
 }
