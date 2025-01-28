dad36dcb94aba8beeadc71053caba8eb62786992
Hans Escobar <hans-escobar@proton.me>
2025-01-28 00:52:48 -0500

    feat: solved exercise

--- exercises/_exercism/25_kindergarten-garden/src/lib.rs
+++ exercises/_exercism/25_kindergarten-garden/src/lib.rs
@@ -1,3 +1,35 @@
 pub fn plants(diagram: &str, student: &str) -> Vec<&'static str> {
-    todo!("based on the {diagram}, determine the plants the {student} is responsible for");
+    let student_id = get_student_id(student).unwrap();
+    diagram
+        .split_ascii_whitespace()
+        .take(2)
+        .flat_map(|r| {
+            r.chars()
+                .collect::<Vec<char>>()
+                .chunks(2)
+                .map(|a| [a[0], a[1]])
+                .nth(student_id)
+                .unwrap()
+        })
+        .map(|plant_code| translate_from_code(plant_code).unwrap())
+        .collect()
+}
+
+fn translate_from_code(code: char) -> Option<&'static str> {
+    match code {
+        'G' => Some("grass"),
+        'C' => Some("clover"),
+        'R' => Some("radishes"),
+        'V' => Some("violets"),
+        _ => None,
+    }
+}
+
+fn get_student_id(student: &str) -> Option<usize> {
+    [
+        "Alice", "Bob", "Charlie", "David", "Eve", "Fred", "Ginny", "Harriet", "Ileana", "Joseph",
+        "Kincaid", "Larry",
+    ]
+    .iter()
+    .position(|s| s == &student)
 }
