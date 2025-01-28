1a7008512b9e89a421d49b8c3f068ffdac18d86d
Hans Escobar <hans-escobar@proton.me>
2025-01-28 17:39:09 -0500

    test: enabled all test

--- exercises/_exercism/27_acronym/src/lib.rs
+++ exercises/_exercism/27_acronym/src/lib.rs
@@ -1,3 +1,29 @@
+use std::fmt::Write;
+
 pub fn abbreviate(phrase: &str) -> String {
-    todo!("Given the phrase '{phrase}', return its acronym");
+    let mut buffer = String::new();
+    for word in phrase.split(&['-', ' ']) {
+        let letters = if word.chars().any(|c| c.is_alphabetic() && c.is_lowercase()) {
+            word.chars()
+                .filter(|c| c.is_alphabetic())
+                .enumerate()
+                .filter_map(|(index, c)| {
+                    if index == 0 || c.is_uppercase() {
+                        Some(c)
+                    } else {
+                        None
+                    }
+                })
+                .collect::<String>()
+                .to_uppercase()
+        } else {
+            match word.chars().find(|c| c.is_uppercase()) {
+                Some(c) => c.to_string(),
+                None => "".to_string(),
+            }
+        };
+
+        write!(&mut buffer, "{}", letters).unwrap();
+    }
+    buffer
 }
