6564df6891d96a59e4bbaf15c08725c4dfb0da12
Hans Escobar <hans-escobar@proton.me>
2025-01-27 23:07:53 -0500

    feat: solved exercise for big big integers

--- exercises/_exercism/23_diffie-hellman/src/lib.rs
+++ exercises/_exercism/23_diffie-hellman/src/lib.rs
@@ -12,13 +12,18 @@ pub fn secret(p: u64, b_pub: u64, a: u64) -> u64 {
     modular_exp(b_pub, a, p)
 }
 
-fn modular_exp(base: u64, exponent: u64, modulus: u64) -> u64 {
+fn modular_exp(base: u64, mut exponent: u64, modulus: u64) -> u64 {
     if modulus == 1 {
         return 0;
     }
     let mut result = 1;
-    for _ in 0..exponent {
-        result = (base * result).rem_euclid(modulus)
+    let mut base: u128 = base.rem_euclid(modulus).into();
+    while exponent > 0 {
+        if exponent.rem_euclid(2) == 1 {
+            result = (result * base).rem_euclid(modulus as u128)
+        }
+        exponent >>= 1;
+        base = (base * base).rem_euclid(modulus as u128);
     }
-    result
+    u64::try_from(result).unwrap()
 }
