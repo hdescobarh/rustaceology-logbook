117e2511731276f612bc66220f026d3ddb98ff22
Hans Escobar <hans-escobar@proton.me>
2025-01-28 01:27:47 -0500

    feat: exercise solved

--- exercises/_exercism/26_eliuds-eggs/src/lib.rs
+++ exercises/_exercism/26_eliuds-eggs/src/lib.rs
@@ -1,3 +1,8 @@
-pub fn egg_count(display_value: u32) -> usize {
-    todo!("count the eggs in {display_value}")
+pub fn egg_count(mut display_value: u32) -> usize {
+    let mut counter = 0;
+    while display_value > 1 {
+        counter += display_value % 2;
+        display_value /= 2
+    }
+    (counter + display_value) as usize
 }
