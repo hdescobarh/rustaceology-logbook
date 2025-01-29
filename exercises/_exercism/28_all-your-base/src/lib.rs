d5042d788c95515f22250e44899cb76e5ee40e42
Hans Escobar <hans-escobar@proton.me>
2025-01-29 10:12:40 -0500

    feat: exercise solved

--- exercises/_exercism/28_all-your-base/src/lib.rs
+++ exercises/_exercism/28_all-your-base/src/lib.rs
@@ -5,36 +5,6 @@ pub enum Error {
     InvalidDigit(u32),
 }
 
-///
-/// Convert a number between two bases.
-///
-/// A number is any slice of digits.
-/// A digit is any unsigned integer (e.g. u8, u16, u32, u64, or usize).
-/// Bases are specified as unsigned integers.
-///
-/// Return the corresponding Error enum if the conversion is impossible.
-///
-///
-/// You are allowed to change the function signature as long as all test still pass.
-///
-///
-/// Example:
-/// Input
-///   number: &[4, 2]
-///   from_base: 10
-///   to_base: 2
-/// Result
-///   Ok(vec![1, 0, 1, 0, 1, 0])
-///
-/// The example corresponds to converting the number 42 from decimal
-/// which is equivalent to 101010 in binary.
-///
-///
-/// Notes:
-///  * The empty slice ( "[]" ) is equal to the number 0.
-///  * Never output leading 0 digits, unless the input number is 0, in which the output must be `[0]`.
-///    However, your function must be able to process input with leading 0 digits.
-///
 pub fn convert(number: &[u32], from_base: u32, to_base: u32) -> Result<Vec<u32>, Error> {
     if let Some(&d) = number.iter().find(|d| **d >= from_base) {
         return Err(Error::InvalidDigit(d));
@@ -48,9 +18,32 @@ pub fn convert(number: &[u32], from_base: u32, to_base: u32) -> Result<Vec<u32>,
         return Err(Error::InvalidOutputBase);
     }
 
-    if number.is_empty() {
-        return Ok(vec![0]);
-    }
+    let result = base_from_integer_value(get_integer_value(number, from_base), to_base);
 
-    todo!("Convert {number:?} from base {from_base} to base {to_base}")
+    if result.is_empty() {
+        Ok(vec![0])
+    } else {
+        Ok(result)
+    }
+}
+
+fn get_integer_value(number: &[u32], from_base: u32) -> u128 {
+    let mut result = 0;
+    for (position, &value) in number.iter().rev().enumerate() {
+        result += value as u128 * (from_base as u128).pow(position as u32)
+    }
+    result
+}
+
+fn base_from_integer_value(mut value: u128, to_base: u32) -> Vec<u32> {
+    let mut result = Vec::new();
+    while value >= to_base as u128 {
+        result.push((value % to_base as u128) as u32);
+        value /= to_base as u128
+    }
+    if value != 0 {
+        result.push(value as u32)
+    }
+    result.reverse();
+    result
 }
