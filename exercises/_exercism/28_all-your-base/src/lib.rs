a36e62a3639df5b43c5cac6f551ad651dd9ca542
Hans Escobar <hans-escobar@proton.me>
2025-01-29 11:21:45 -0500

    refactor: simplified instructions

--- exercises/_exercism/28_all-your-base/src/lib.rs
+++ exercises/_exercism/28_all-your-base/src/lib.rs
@@ -6,44 +6,39 @@ pub enum Error {
 }
 
 pub fn convert(number: &[u32], from_base: u32, to_base: u32) -> Result<Vec<u32>, Error> {
-    if let Some(&d) = number.iter().find(|d| **d >= from_base) {
-        return Err(Error::InvalidDigit(d));
-    }
-
-    if [0, 1].contains(&from_base) {
+    if from_base <= 1 {
         return Err(Error::InvalidInputBase);
-    }
-
-    if [0, 1].contains(&to_base) {
+    } else if to_base <= 1 {
         return Err(Error::InvalidOutputBase);
     }
 
-    let result = base_from_integer_value(get_integer_value(number, from_base), to_base);
-
-    if result.is_empty() {
-        Ok(vec![0])
-    } else {
-        Ok(result)
-    }
+    Ok(base_from_integer_value(
+        get_integer_value(number, from_base)?,
+        to_base,
+    ))
 }
 
-fn get_integer_value(number: &[u32], from_base: u32) -> u128 {
+fn get_integer_value(number: &[u32], from_base: u32) -> Result<u128, Error> {
     let mut result = 0;
-    for (position, &value) in number.iter().rev().enumerate() {
-        result += value as u128 * (from_base as u128).pow(position as u32)
+    for &digit in number.iter() {
+        if digit >= from_base {
+            return Err(Error::InvalidDigit(digit));
+        }
+        result = result * from_base as u128 + digit as u128
     }
-    result
+    Ok(result)
 }
 
 fn base_from_integer_value(mut value: u128, to_base: u32) -> Vec<u32> {
     let mut result = Vec::new();
-    while value >= to_base as u128 {
+    while value > 0 {
         result.push((value % to_base as u128) as u32);
         value /= to_base as u128
     }
-    if value != 0 {
-        result.push(value as u32)
-    }
     result.reverse();
-    result
+    if result.is_empty() {
+        vec![0]
+    } else {
+        result
+    }
 }
