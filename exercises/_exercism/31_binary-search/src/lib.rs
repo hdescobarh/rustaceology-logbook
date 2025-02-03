46c5f44ebc54142e4997c452d8dd0ed5fc89c7fe
Hans Escobar <hans-escobar@proton.me>
2025-02-02 22:51:16 -0500

    feat: generalized function signature with generics

--- exercises/_exercism/31_binary-search/src/lib.rs
+++ exercises/_exercism/31_binary-search/src/lib.rs
@@ -1,5 +1,3 @@
-pub fn find(array: &[i32], key: i32) -> Option<usize> {
-    todo!(
-        "Using the binary search algorithm, find the element '{key}' in the array '{array:?}' and return its index."
-    );
+pub fn find<T: PartialOrd>(array: &[T], key: T) -> Option<usize> {
+    todo!();
 }
