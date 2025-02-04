b2f8e086e61bf8687ac60fd4b98ec5868ce5d091
Hans Escobar <hans-escobar@proton.me>
2025-02-03 20:40:31 -0500

    Revert "feat: added Frame struct"
    This reverts commit 09e5194bc874a50597f5e5c5ab63c5c741c72956.

--- exercises/_exercism/32_bowling/src/lib.rs
+++ exercises/_exercism/32_bowling/src/lib.rs
@@ -1,61 +1,10 @@
-pub enum FrameStatus {
-    Unbegun,
-    Unfinished(u16),
-    Open(u16),
-    Spare(u16),
-    Strike,
-}
-
-struct Frame {
-    status: FrameStatus,
-    fill_balls: Option<Vec<u16>>,
-}
-
-impl Frame {
-    pub fn roll(&mut self, pins: u16) -> Result<(), Error> {
-        if pins > 10 {
-            return Err(Error::NotEnoughPinsLeft);
-        }
-        match self.status {
-            FrameStatus::Unbegun => {
-                self.status = if pins == 10 {
-                    FrameStatus::Strike
-                } else {
-                    FrameStatus::Unfinished(pins)
-                }
-            }
-            FrameStatus::Unfinished(knocked) => {
-                self.status = match knocked.checked_add(pins) {
-                    Some(new_knocked) if new_knocked < 10 => FrameStatus::Open(new_knocked),
-                    Some(10) => FrameStatus::Spare(pins),
-                    _ => return Err(Error::NotEnoughPinsLeft),
-                }
-            }
-            FrameStatus::Spare(_) if self.fill_balls.as_ref().is_some_and(|v| v.is_empty()) => {
-                if let Some(v) = self.fill_balls.as_mut() {
-                    v.push(pins);
-                };
-            }
-            FrameStatus::Strike if self.fill_balls.as_ref().is_some_and(|v| v.len() < 2) => {
-                if let Some(v) = self.fill_balls.as_mut() {
-                    v.push(pins);
-                }
-            }
-            _ => return Err(Error::NotEnoughPinsLeft),
-        };
-        Ok(())
-    }
-}
-
 #[derive(Debug, PartialEq, Eq)]
 pub enum Error {
     NotEnoughPinsLeft,
     GameComplete,
 }
-pub struct BowlingGame {
-    current: u8,
-    frames: Vec<Frame>,
-}
+
+pub struct BowlingGame {}
 
 impl BowlingGame {
     pub fn new() -> Self {
