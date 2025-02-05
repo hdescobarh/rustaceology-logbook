9618877bbb0df65008fd563219d546aee416f295
Hans Escobar <hans-escobar@proton.me>
2025-02-05 15:45:40 -0500

    feat: now can create Edges and Nodes

--- exercises/_exercism/33_dot-dsl/src/graph/graph_items/edge.rs
+++ exercises/_exercism/33_dot-dsl/src/graph/graph_items/edge.rs
@@ -1,20 +1,30 @@
 use std::collections::HashMap;
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub struct Edge {
+    label_1: String,
+    label_2: String,
     attributes: HashMap<String, String>,
 }
 
 impl Edge {
-    pub fn new(node_1: &str, node_2: &str) -> Self {
-        todo!();
+    pub fn new(label_1: &str, label_2: &str) -> Self {
+        Self {
+            label_1: label_1.to_string(),
+            label_2: label_2.to_string(),
+            attributes: HashMap::new(),
+        }
     }
 
-    pub fn with_attrs(self, attrs: &[(&str, &str)]) -> Self {
-        todo!()
+    pub fn with_attrs(mut self, attrs: &[(&str, &str)]) -> Self {
+        for (name, value) in attrs {
+            self.attributes.insert(name.to_string(), value.to_string());
+        }
+
+        self
     }
 
     pub fn attr(&self, attribute: &str) -> Option<&str> {
-        todo!()
+        self.attributes.get(attribute).map(|s| s.as_str())
     }
 }
--- exercises/_exercism/33_dot-dsl/src/graph/graph_items/node.rs
+++ exercises/_exercism/33_dot-dsl/src/graph/graph_items/node.rs
@@ -1,14 +1,30 @@
-#[derive(Debug, PartialEq)]
-pub struct Node {}
+use std::collections::HashMap;
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct Node {
+    label: String,
+    attributes: HashMap<String, String>,
+}
 impl Node {
-    pub fn new(name: &str) -> Self {
-        todo!();
+    pub fn new(label: &str) -> Self {
+        Self {
+            label: label.to_string(),
+            attributes: HashMap::new(),
+        }
     }
-    pub fn with_attrs(self, attrs: &[(&str, &str)]) -> Self {
-        todo!()
+    pub fn with_attrs(mut self, attrs: &[(&str, &str)]) -> Self {
+        for (attribute, value) in attrs {
+            self.attributes
+                .insert(attribute.to_string(), value.to_string());
+        }
+        self
     }
 
     pub fn attr(&self, attribute: &str) -> Option<&str> {
-        todo!()
+        self.attributes.get(attribute).map(|v| v.as_str())
+    }
+
+    pub fn label(&self) -> &str {
+        self.attributes.get("label").unwrap_or(&self.label)
     }
 }
