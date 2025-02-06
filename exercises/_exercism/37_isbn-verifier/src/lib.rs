439b429fbfc9e1d09fcccc1df2f29c6a81fe8be0
Hans Escobar <hans-escobar@proton.me>
2025-02-06 02:03:27 -0500

    refactor: reduced unnecesary complexity. Moved auxiliar function match to is_valid_isbn

--- exercises/_exercism/37_isbn-verifier/src/lib.rs
+++ exercises/_exercism/37_isbn-verifier/src/lib.rs
@@ -2,18 +2,11 @@ pub fn is_valid_isbn(isbn: &str) -> bool {
     let (mut total, mut is_complete) = (0, false);
     for (index, letter) in isbn.chars().filter(|c| *c != '-').rev().enumerate() {
         is_complete |= index == 9;
-        match parse(index as u32, letter) {
-            Some(value) => total += value,
-            None => return false,
+        match (index, letter.to_digit(10)) {
+            (0, None) if letter == 'X' => total += 10 * (index as u32 + 1),
+            (0..=9, Some(d)) => total += d * (index as u32 + 1),
+            _ => return false,
         }
     }
     total % 11 == 0 && is_complete
-}
-
-fn parse(index: u32, letter: char) -> Option<u32> {
-    match (index, letter.to_digit(10)) {
-        (0, None) if letter == 'X' => Some(10 * (index + 1)),
-        (0..=9, Some(d)) => Some(d * (index + 1)),
-        _ => None,
-    }
 }
