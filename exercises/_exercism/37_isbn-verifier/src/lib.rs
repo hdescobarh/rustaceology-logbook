baa1b7788927f9a106d8e44a0ba3984f4f742e76
Hans Escobar <hans-escobar@proton.me>
2025-02-06 01:36:33 -0500

    refactor: improved readability

--- exercises/_exercism/37_isbn-verifier/src/lib.rs
+++ exercises/_exercism/37_isbn-verifier/src/lib.rs
@@ -1,25 +1,18 @@
 pub fn is_valid_isbn(isbn: &str) -> bool {
-    let mut total = 0;
-    let mut last_index = 0;
-    for (index, c) in isbn.chars().filter(|c| *c != '-').rev().enumerate() {
-        last_index = index;
-        match parse(c, index as u32) {
+    let (mut total, mut last) = (0, 0);
+    for (index, letter) in isbn.chars().filter(|c| *c != '-').rev().enumerate() {
+        last = index;
+        match parse(index as u32, letter) {
             Some(value) => total += value,
             None => return false,
         }
     }
-    total % 11 == 0 && last_index == 9
+    total % 11 == 0 && last == 9
 }
 
-fn parse(c: char, index: u32) -> Option<u32> {
-    match (index, c.to_digit(10)) {
-        (0, None) => {
-            if c == 'X' {
-                Some(10 * (index + 1))
-            } else {
-                None
-            }
-        }
+fn parse(index: u32, letter: char) -> Option<u32> {
+    match (index, letter.to_digit(10)) {
+        (0, None) if letter == 'X' => Some(10 * (index + 1)),
         (index, Some(d)) if index <= 9 => Some(d * (index + 1)),
         _ => None,
     }
