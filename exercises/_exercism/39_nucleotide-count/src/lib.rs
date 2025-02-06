4b91e1f874fc5687b4976c401e076df797123377
Hans Escobar <hans-escobar@proton.me>
2025-02-06 14:58:45 -0500

    feat: solved exercise

--- exercises/_exercism/39_nucleotide-count/src/lib.rs
+++ exercises/_exercism/39_nucleotide-count/src/lib.rs
@@ -1,9 +1,30 @@
 use std::collections::HashMap;
 
+const DNA_NUCLEOTIDES: [char; 4] = ['A', 'G', 'C', 'T'];
+
 pub fn count(nucleotide: char, dna: &str) -> Result<usize, char> {
-    todo!("How much of nucleotide type '{nucleotide}' is contained inside DNA string '{dna}'?");
+    let mut result = if DNA_NUCLEOTIDES.contains(&nucleotide) {
+        0
+    } else {
+        return Err(nucleotide);
+    };
+    for c in dna.chars() {
+        if c == nucleotide {
+            result += 1
+        } else if !DNA_NUCLEOTIDES.contains(&c) {
+            return Err(c);
+        }
+    }
+    Ok(result)
 }
 
 pub fn nucleotide_counts(dna: &str) -> Result<HashMap<char, usize>, char> {
-    todo!("How much of every nucleotide type is contained inside DNA string '{dna}'?");
+    let mut result = HashMap::from_iter(DNA_NUCLEOTIDES.iter().map(|&c| (c, 0)));
+    for c in dna.chars() {
+        match result.get_mut(&c) {
+            Some(d) => *d += 1,
+            None => return Err(c),
+        }
+    }
+    Ok(result)
 }
