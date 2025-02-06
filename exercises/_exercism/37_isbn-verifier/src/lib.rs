682af3206468fe6651187ff5402ddbc95cd6d1e3
Hans Escobar <hans-escobar@proton.me>
2025-02-06 02:00:14 -0500

    reactor: now check if it is complete in the loop

--- exercises/_exercism/37_isbn-verifier/src/lib.rs
+++ exercises/_exercism/37_isbn-verifier/src/lib.rs
@@ -1,19 +1,19 @@
 pub fn is_valid_isbn(isbn: &str) -> bool {
-    let (mut total, mut last) = (0, 0);
+    let (mut total, mut is_complete) = (0, false);
     for (index, letter) in isbn.chars().filter(|c| *c != '-').rev().enumerate() {
-        last = index;
+        is_complete |= index == 9;
         match parse(index as u32, letter) {
             Some(value) => total += value,
             None => return false,
         }
     }
-    total % 11 == 0 && last == 9
+    total % 11 == 0 && is_complete
 }
 
 fn parse(index: u32, letter: char) -> Option<u32> {
     match (index, letter.to_digit(10)) {
         (0, None) if letter == 'X' => Some(10 * (index + 1)),
-        (index, Some(d)) if index <= 9 => Some(d * (index + 1)),
+        (0..=9, Some(d)) => Some(d * (index + 1)),
         _ => None,
     }
 }
