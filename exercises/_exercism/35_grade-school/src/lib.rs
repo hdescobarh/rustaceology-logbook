3c5318adfcb94c37762ea209daec5956d6c4198c
Hans Escobar <hans-escobar@proton.me>
2025-02-05 22:35:55 -0500

    feat: exercise solved

--- exercises/_exercism/35_grade-school/src/lib.rs
+++ exercises/_exercism/35_grade-school/src/lib.rs
@@ -1,23 +1,93 @@
-pub struct School {}
+use std::collections::BTreeMap;
+
+#[derive(Clone)]
+struct StudentsList {
+    grade: u32,
+    students: Vec<String>, // always sorted
+}
+
+impl StudentsList {
+    pub fn new(grade: u32) -> Self {
+        Self {
+            grade,
+            students: Vec::new(),
+        }
+    }
+
+    pub fn with_initial_student(mut self, student: &str) -> Self {
+        if self.students.is_empty() {
+            self.students.push(student.to_string())
+        }
+        self
+    }
+
+    pub fn add(&mut self, student: &str) -> Result<(), String> {
+        let student = student.to_string();
+        if self.students.is_empty() {
+            self.students.push(student);
+            return Ok(());
+        }
+        match self.students.binary_search(&student) {
+            Ok(_) => {
+                return Err(format!(
+                    "Student {student} already is in grade {grade}",
+                    grade = self.grade
+                ))
+            }
+            Err(index) => self.students.insert(index, student),
+        };
+        Ok(())
+    }
+
+    pub fn contains(&self, student: &str) -> bool {
+        self.students.binary_search(&student.to_string()).is_ok()
+    }
+}
+
+impl From<StudentsList> for Vec<String> {
+    fn from(value: StudentsList) -> Self {
+        value.students
+    }
+}
+
+pub struct School {
+    roster: BTreeMap<u32, StudentsList>,
+}
 
 impl School {
     pub fn new() -> School {
-        todo!()
+        Self {
+            roster: BTreeMap::new(),
+        }
     }
 
-    pub fn add(&mut self, grade: u32, student: &str) {
-        todo!("Add {student} to the roster for {grade}")
+    pub fn add(&mut self, grade: u32, student: &str) -> Result<(), String> {
+        for (grade, list) in self.roster.iter() {
+            if list.contains(student) {
+                return Err(format!("Student {student} already is in grade {grade}"));
+            }
+        }
+
+        match self.roster.get_mut(&grade) {
+            Some(list) => list.add(student),
+            None => {
+                self.roster.insert(
+                    grade,
+                    StudentsList::new(grade).with_initial_student(student),
+                );
+                Ok(())
+            }
+        }
     }
 
     pub fn grades(&self) -> Vec<u32> {
-        todo!()
+        self.roster.keys().cloned().collect()
     }
 
-    // If `grade` returned a reference, `School` would be forced to keep a `Vec<String>`
-    // internally to lend out. By returning an owned vector of owned `String`s instead,
-    // the internal structure can be completely arbitrary. The tradeoff is that some data
-    // must be copied each time `grade` is called.
     pub fn grade(&self, grade: u32) -> Vec<String> {
-        todo!("Return the list of students in {grade}")
+        self.roster
+            .get(&grade)
+            .map(|list| Vec::from(list.clone()))
+            .unwrap_or_default()
     }
 }
