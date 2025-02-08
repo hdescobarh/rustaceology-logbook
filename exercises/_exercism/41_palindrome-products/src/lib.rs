05a177cdc1de3ee05c27859f647db7b1ec148ffc
Hans Escobar <hans-escobar@proton.me>
2025-02-08 15:17:21 -0500

    refactor: removed old implementation methods

--- exercises/_exercism/41_palindrome-products/src/lib.rs
+++ exercises/_exercism/41_palindrome-products/src/lib.rs
@@ -1,4 +1,3 @@
-use std::collections::BTreeMap;
 use std::collections::HashSet;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -21,37 +20,6 @@ impl Palindrome {
     pub fn into_factors(self) -> HashSet<(u64, u64)> {
         self.factors
     }
-
-    fn is_palindrome(value: u64) -> bool {
-        let mut reverse = value;
-        let mut result = 0;
-        while reverse > 0 {
-            result = result * 10 + reverse % 10;
-            reverse /= 10;
-        }
-        result == value
-    }
-}
-
-pub fn _palindrome_products(min: u64, max: u64) -> Option<(Palindrome, Palindrome)> {
-    let mut candidates: BTreeMap<u64, Vec<(u64, u64)>> = BTreeMap::new();
-    (min..=max)
-        .flat_map(|j| (min..=j).map(move |i| (i * j, (i, j))))
-        .filter(|(product, _)| Palindrome::is_palindrome(*product))
-        .for_each(|(product, factors)| {
-            candidates
-                .entry(product)
-                .and_modify(|f| f.push(factors))
-                .or_insert(vec![factors]);
-        });
-
-    let min_palindrome = Palindrome::new(candidates.pop_first()?);
-    let max_palindrome = candidates
-        .pop_last()
-        .map(Palindrome::new)
-        .unwrap_or(min_palindrome.clone());
-
-    Some((min_palindrome, max_palindrome))
 }
 
 pub fn palindrome_products(min: u64, max: u64) -> Option<(Palindrome, Palindrome)> {
@@ -176,25 +144,6 @@ impl PalindromeOperator {
 #[cfg(test)]
 mod test {
     use crate::{Palindrome, PalindromeOperator};
-
-    #[test]
-    fn check_is_palindrome() {
-        let cases = [
-            (0, true),
-            (9, true),
-            (18, false),
-            (121, true),
-            (9009, true),
-            (123321, true),
-            (1234321, true),
-            (321321, false),
-            (3214321, false),
-        ];
-
-        for (input, expected) in cases {
-            assert_eq!(Palindrome::is_palindrome(input), expected)
-        }
-    }
 
     #[test]
     fn next_palindrome_from_odd_to_same_length() {
