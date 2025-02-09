b793f5be4d7aea3c9747b1bc639e4102053fd6eb
Hans Escobar <hans-escobar@proton.me>
2025-02-08 22:44:10 -0500

    refactor: improved speed with prime factor decomposition

--- exercises/_exercism/44_perfect-numbers/src/lib.rs
+++ exercises/_exercism/44_perfect-numbers/src/lib.rs
@@ -1,3 +1,5 @@
+// Improved speed using prime factor decomposition O(sqrt(n))
+
 #[derive(Debug, PartialEq, Eq)]
 pub enum Classification {
     Abundant,
@@ -6,13 +8,33 @@ pub enum Classification {
 }
 
 pub fn classify(num: u64) -> Option<Classification> {
-    if num == 0 {
-        return None;
-    }
-    let aliquot_sum: u64 = (1..num).filter(|n| num % n == 0).sum();
+    let aliquot_sum: u64 = sigma(num as u128)? - num;
     Some(match num.cmp(&aliquot_sum) {
         std::cmp::Ordering::Less => Classification::Abundant,
         std::cmp::Ordering::Equal => Classification::Perfect,
         std::cmp::Ordering::Greater => Classification::Deficient,
     })
+}
+
+// Calculates sigma [OEIS: A000203] and performs prime factor decomposition at the same time
+fn sigma(num: u128) -> Option<u64> {
+    if num == 0 {
+        return None;
+    }
+    let (mut product, mut remaining) = (1, num);
+    [2].into_iter()
+        .chain((3..=num.isqrt() + 1).step_by(2))
+        .for_each(|x| {
+            let mut sum = 0;
+            while remaining % x == 0 {
+                sum = x * sum + x;
+                remaining /= x;
+            }
+            product *= sum + 1;
+        });
+
+    if remaining > 2 {
+        product *= remaining + 1;
+    }
+    u64::try_from(product).ok()
 }
