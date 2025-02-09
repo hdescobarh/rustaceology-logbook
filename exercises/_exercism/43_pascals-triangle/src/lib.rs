4f9d6703a18a13b0b17cfb6ad9a965417494080b
Hans Escobar <hans-escobar@proton.me>
2025-02-08 19:04:42 -0500

    feat: exercise solved

--- exercises/_exercism/43_pascals-triangle/src/lib.rs
+++ exercises/_exercism/43_pascals-triangle/src/lib.rs
@@ -1,11 +1,27 @@
-pub struct PascalsTriangle;
+pub struct PascalsTriangle(Vec<Vec<u32>>);
 
 impl PascalsTriangle {
     pub fn new(row_count: u32) -> Self {
-        todo!("create Pascal's triangle with {row_count} rows");
+        Self(Vec::with_capacity(row_count as usize)).fill(row_count as usize)
     }
 
     pub fn rows(&self) -> Vec<Vec<u32>> {
-        todo!();
+        self.0.clone()
+    }
+
+    fn fill(mut self, row_count: usize) -> Self {
+        while self.0.len() < row_count {
+            if self.0.is_empty() {
+                self.0.push(vec![1]);
+                continue;
+            }
+            let next_row: Vec<u32> = [1]
+                .into_iter()
+                .chain(self.0.last().unwrap().windows(2).map(|v| v[0] + v[1]))
+                .chain([1].into_iter())
+                .collect();
+            self.0.push(next_row)
+        }
+        self
     }
 }
