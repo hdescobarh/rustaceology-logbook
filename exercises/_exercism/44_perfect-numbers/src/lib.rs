97a6870439c22402bd9b4a0bced8298f95d08a93
Hans Escobar <hans-escobar@proton.me>
2025-02-08 19:25:30 -0500

    feat: exercise solved

--- exercises/_exercism/44_perfect-numbers/src/lib.rs
+++ exercises/_exercism/44_perfect-numbers/src/lib.rs
@@ -6,5 +6,13 @@ pub enum Classification {
 }
 
 pub fn classify(num: u64) -> Option<Classification> {
-    todo!("classify {num}");
+    if num == 0 {
+        return None;
+    }
+    let aliquot_sum: u64 = (1..num).filter(|n| num % n == 0).sum();
+    Some(match num.cmp(&aliquot_sum) {
+        std::cmp::Ordering::Less => Classification::Abundant,
+        std::cmp::Ordering::Equal => Classification::Perfect,
+        std::cmp::Ordering::Greater => Classification::Deficient,
+    })
 }
