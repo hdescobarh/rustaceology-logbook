7a1762b1f89552234d70a26b90900fdf1fb7d02a
Hans Escobar <hans-escobar@proton.me>
2025-02-09 14:18:30 -0500

    feat: implemented encode function

--- exercises/_exercism/48_run-length-encoding/src/lib.rs
+++ exercises/_exercism/48_run-length-encoding/src/lib.rs
@@ -1,5 +1,39 @@
+use std::iter::repeat;
+
 pub fn encode(source: &str) -> String {
-    todo!("Return the run-length encoding of {source}.");
+    // source contains only alphabetic ASCII
+    let mut output = String::new();
+    let mut previous = None::<char>;
+    let mut count = 0;
+
+    for current in source.chars() {
+        match previous {
+            Some(v) if v == current => count += 1,
+            Some(v) => {
+                let encoded = if count > 1 {
+                    format!("{count}{v}")
+                } else {
+                    format!("{v}")
+                };
+                output.push_str(&encoded);
+                previous = Some(current);
+                count = 1;
+            }
+            None => {
+                previous = Some(current);
+                count += 1;
+            }
+        };
+    }
+    if let Some(v) = previous {
+        let encoded = if count > 1 {
+            format!("{count}{v}")
+        } else {
+            format!("{v}")
+        };
+        output.push_str(&encoded);
+    }
+    output
 }
 
 pub fn decode(source: &str) -> String {
