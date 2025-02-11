e585a9c8e105a5841670121fda8071adb167012b
Hans Escobar <hans-escobar@proton.me>
2025-02-11 11:01:45 -0500

    test: enabled all tests

--- exercises/_exercism/49_saddle-points/tests/saddle-points.rs
+++ exercises/_exercism/49_saddle-points/tests/saddle-points.rs
@@ -2,7 +2,10 @@ use saddle_points::*;
 
 #[test]
 fn can_identify_single_saddle_point() {
-    let input = &[vec![9, 8, 7], vec![5, 3, 2], vec![6, 6, 7]];
+    let input = &[
+        vec![9, 8, 7],
+        vec![5, 3, 2],
+        vec![6, 6, 7]];
     let mut output = find_saddle_points(input);
     output.sort_unstable();
     let expected = &[(1, 0)];
@@ -10,7 +13,6 @@ fn can_identify_single_saddle_point() {
 }
 
 #[test]
-#[ignore]
 fn can_identify_that_empty_matrix_has_no_saddle_points() {
     let input = &[vec![]];
     let mut output = find_saddle_points(input);
@@ -20,9 +22,11 @@ fn can_identify_that_empty_matrix_has_no_saddle_points() {
 }
 
 #[test]
-#[ignore]
 fn can_identify_lack_of_saddle_points_when_there_are_none() {
-    let input = &[vec![1, 2, 3], vec![3, 1, 2], vec![2, 3, 1]];
+    let input = &[
+        vec![1, 2, 3],
+        vec![3, 1, 2],
+        vec![2, 3, 1]];
     let mut output = find_saddle_points(input);
     output.sort_unstable();
     let expected = &[];
@@ -30,7 +34,6 @@ fn can_identify_lack_of_saddle_points_when_there_are_none() {
 }
 
 #[test]
-#[ignore]
 fn can_identify_multiple_saddle_points_in_a_column() {
     let input = &[vec![4, 5, 4], vec![3, 5, 5], vec![1, 5, 4]];
     let mut output = find_saddle_points(input);
@@ -40,7 +43,6 @@ fn can_identify_multiple_saddle_points_in_a_column() {
 }
 
 #[test]
-#[ignore]
 fn can_identify_multiple_saddle_points_in_a_row() {
     let input = &[vec![6, 7, 8], vec![5, 5, 5], vec![7, 5, 6]];
     let mut output = find_saddle_points(input);
@@ -50,7 +52,6 @@ fn can_identify_multiple_saddle_points_in_a_row() {
 }
 
 #[test]
-#[ignore]
 fn can_identify_saddle_point_in_bottom_right_corner() {
     let input = &[vec![8, 7, 9], vec![6, 7, 6], vec![3, 2, 5]];
     let mut output = find_saddle_points(input);
@@ -60,7 +61,6 @@ fn can_identify_saddle_point_in_bottom_right_corner() {
 }
 
 #[test]
-#[ignore]
 fn can_identify_saddle_points_in_a_non_square_matrix() {
     let input = &[vec![3, 1, 3], vec![3, 2, 4]];
     let mut output = find_saddle_points(input);
@@ -70,7 +70,6 @@ fn can_identify_saddle_points_in_a_non_square_matrix() {
 }
 
 #[test]
-#[ignore]
 fn can_identify_that_saddle_points_in_a_single_column_matrix_are_those_with_the_minimum_value() {
     let input = &[vec![2], vec![1], vec![4], vec![1]];
     let mut output = find_saddle_points(input);
@@ -80,7 +79,6 @@ fn can_identify_that_saddle_points_in_a_single_column_matrix_are_those_with_the_
 }
 
 #[test]
-#[ignore]
 fn can_identify_that_saddle_points_in_a_single_row_matrix_are_those_with_the_maximum_value() {
     let input = &[vec![2, 5, 3, 5]];
     let mut output = find_saddle_points(input);
