9cc006e55b8427772154a7e89896cf506853a1b4
Hans Escobar <hans-escobar@proton.me>
2025-02-11 11:17:50 -0500

    refactor: improved variable names and readability

--- exercises/_exercism/49_saddle-points/src/lib.rs
+++ exercises/_exercism/49_saddle-points/src/lib.rs
@@ -5,14 +5,14 @@ pub fn find_saddle_points(input: &[Vec<u64>]) -> Vec<Coordinate> {
     } else {
         (input.len(), input[0].len())
     };
-
-    let tallest: Vec<u64> = (0..rows).map(|i| *input[i].iter().max().unwrap()).collect();
-    let shortest: Vec<u64> = (0..cols)
-        .map(|j| (0..rows).map(|i| input[i][j]).min().unwrap())
+    let tallest_size_by_row: Vec<u64> =
+        (0..rows).map(|i| *input[i].iter().max().unwrap()).collect();
+    let shortest_size_by_col: Vec<u64> = (0..cols)
+        .map(|j| input.iter().map(|row| row[j]).min().unwrap())
         .collect();
     let output: Vec<Coordinate> = (0..rows)
         .flat_map(|i| (0..cols).map(move |j| (i, j)))
-        .filter(|&(i, j)| tallest[i] == shortest[j])
+        .filter(|&(i, j)| tallest_size_by_row[i] == shortest_size_by_col[j])
         .collect();
 
     if output.is_empty() {
