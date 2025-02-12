5bab959049d3b80e2fc6f39dff9b371eef4ab39f
Hans Escobar <hans-escobar@proton.me>
2025-02-12 11:07:21 -0500

    feat: partial solution. Pending to solve bug with thousands

--- exercises/_exercism/50_exercise=say/src/lib.rs
+++ exercises/_exercism/50_exercise=say/src/lib.rs
@@ -1,3 +1,124 @@
-pub fn encode(n: u64) -> String {
-    todo!("Say {n} in English.");
+pub fn encode(mut n: u64) -> String {
+    let mut current = 0;
+    let mut periods: Vec<Period> = Vec::new();
+    loop {
+        periods.push(Period::new(n % 1000, current).unwrap());
+        n /= 1000;
+        current += 1;
+        if n == 0 {
+            break;
+        }
+    }
+    periods
+        .into_iter()
+        .map(String::from)
+        .collect::<Vec<String>>()
+        .join(" ")
+}
+
+struct Period {
+    name: u8,
+    ones: u8,
+    tens: u8,
+    hundreds: u8,
+}
+
+impl Period {
+    pub fn new(mut value: u64, name: u8) -> Result<Self, ()> {
+        if value > 999 || name > 6 {
+            return Err(());
+        }
+        let mut period_places: Vec<u8> = Vec::with_capacity(3);
+        for _ in 0..3 {
+            period_places.push((value % 10) as u8);
+            value /= 10;
+        }
+        Ok(Period {
+            name,
+            ones: period_places[0],
+            tens: period_places[1],
+            hundreds: period_places[2],
+        })
+    }
+
+    fn name_to_word(&self) -> &str {
+        match self.name {
+            1 => " thousand",
+            2 => " million",
+            3 => " billion",
+            4 => " trillion",
+            5 => " quadrillion",
+            6 => " quintillion",
+            _ => "",
+        }
+    }
+
+    fn places_to_word(&self) -> String {
+        match (self.hundreds, self.tens, self.ones) {
+            (0, 0, _) => Self::digit_to_word(self.ones).to_string(),
+            (0, _, _) => self.tens_to_word().to_string(),
+            (_, _, _) => format!(
+                "{} hundred{}",
+                Self::digit_to_word(self.hundreds),
+                self.tens_to_word()
+            ),
+        }
+    }
+
+    fn digit_to_word(digit: u8) -> &'static str {
+        match digit {
+            0 => "zero",
+            1 => "one",
+            2 => "two",
+            3 => "three",
+            4 => "four",
+            5 => "five",
+            6 => "six",
+            7 => "seven",
+            8 => "eight",
+            9 => "nine",
+            _ => "",
+        }
+    }
+
+    fn tens_to_word(&self) -> String {
+        match (self.tens, self.ones) {
+            (1, 0) => " ten".to_string(),
+            (1, 1) => " eleven".to_string(),
+            (1, 2) => " twelve".to_string(),
+            (1, 3) => " thirteen".to_string(),
+            (1, 4) => " fourteen".to_string(),
+            (1, 5) => " fifteen".to_string(),
+            (1, 6) => " sixteen".to_string(),
+            (1, 7) => " seventeen".to_string(),
+            (1, 8) => " eighteen".to_string(),
+            (1, 9) => " nineteen".to_string(),
+            (_, 0) => self.tens_prefix_from_twenty().to_string(),
+            _ => format!(
+                "{}-{}",
+                self.tens_prefix_from_twenty(),
+                Self::digit_to_word(self.ones)
+            ),
+        }
+    }
+
+    fn tens_prefix_from_twenty(&self) -> &str {
+        match self.tens {
+            2 => " twenty",
+            3 => " thirty",
+            4 => " forty",
+            5 => " fifty",
+            6 => " sixty",
+            7 => " seventy",
+            8 => " eighty",
+            9 => " ninety",
+            _ => "",
+        }
+    }
+}
+
+impl From<Period> for String {
+    fn from(value: Period) -> Self {
+        format!("{}{}", value.places_to_word(), value.name_to_word())
+    }
 }
