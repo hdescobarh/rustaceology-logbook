65d807403b9690a0e120c62c681b3eee8b787efa
Hans Escobar <hans-escobar@proton.me>
2025-02-12 15:47:33 -0500

    refactor: change how Period is generated, added null creator

--- exercises/_exercism/50_exercise=say/src/lib.rs
+++ exercises/_exercism/50_exercise=say/src/lib.rs
@@ -1,13 +1,14 @@
 pub fn encode(mut n: u64) -> String {
-    let mut current = 0;
-    let mut periods: Vec<Period> = Vec::new();
-    loop {
-        periods.push(Period::new(n % 1000, current).unwrap());
+    if n == 0 {
+        return Period::null().into();
+    }
+    let (mut current, mut periods) = (0, Vec::new());
+    while n > 0 {
+        if let Some(p) = Period::new(n % 1000, current) {
+            periods.push(p);
+        }
         n /= 1000;
         current += 1;
-        if n == 0 {
-            break;
-        }
     }
     periods
         .into_iter()
@@ -24,21 +25,33 @@ struct Period {
 }
 
 impl Period {
-    pub fn new(mut value: u64, name: u8) -> Result<Self, ()> {
+    pub fn new(mut value: u64, name: u8) -> Option<Self> {
         if value > 999 || name > 6 {
-            return Err(());
+            return None;
         }
         let mut period_places: Vec<u8> = Vec::with_capacity(3);
         for _ in 0..3 {
             period_places.push((value % 10) as u8);
             value /= 10;
         }
-        Ok(Period {
+        if period_places.iter().sum::<u8>() == 0 {
+            return None;
+        }
+        Some(Period {
             name,
             ones: period_places[0],
             tens: period_places[1],
             hundreds: period_places[2],
         })
+    }
+
+    pub fn null() -> Self {
+        Self {
+            name: 0,
+            ones: 0,
+            tens: 0,
+            hundreds: 0,
+        }
     }
 
     fn name_to_word(&self) -> &str {
@@ -83,16 +96,16 @@ impl Period {
 
     fn tens_to_word(&self) -> String {
         match (self.tens, self.ones) {
-            (1, 0) => " ten".to_string(),
-            (1, 1) => " eleven".to_string(),
-            (1, 2) => " twelve".to_string(),
-            (1, 3) => " thirteen".to_string(),
-            (1, 4) => " fourteen".to_string(),
-            (1, 5) => " fifteen".to_string(),
-            (1, 6) => " sixteen".to_string(),
-            (1, 7) => " seventeen".to_string(),
-            (1, 8) => " eighteen".to_string(),
-            (1, 9) => " nineteen".to_string(),
+            (1, 0) => "ten".to_string(),
+            (1, 1) => "eleven".to_string(),
+            (1, 2) => "twelve".to_string(),
+            (1, 3) => "thirteen".to_string(),
+            (1, 4) => "fourteen".to_string(),
+            (1, 5) => "fifteen".to_string(),
+            (1, 6) => "sixteen".to_string(),
+            (1, 7) => "seventeen".to_string(),
+            (1, 8) => "eighteen".to_string(),
+            (1, 9) => "nineteen".to_string(),
             (_, 0) => self.tens_prefix_from_twenty().to_string(),
             _ => format!(
                 "{}-{}",
