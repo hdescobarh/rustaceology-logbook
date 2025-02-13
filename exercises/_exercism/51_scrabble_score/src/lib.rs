c66e85061bf6cb5533f37230eb66c7e2cb21c587
Hans Escobar <hans-escobar@proton.me>
2025-02-13 08:49:31 -0500

    feat: exercise solved

--- exercises/_exercism/51_scrabble_score/src/lib.rs
+++ exercises/_exercism/51_scrabble_score/src/lib.rs
@@ -1,4 +1,17 @@
-/// Compute the Scrabble score for a word.
+const ALPHABET_SCORES: [u64; 26] = [
+    1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10,
+];
+
 pub fn score(word: &str) -> u64 {
-    todo!("Score {word} in Scrabble.");
+    word.chars()
+        .filter(|c| c.is_ascii_alphabetic())
+        .fold(0, |acc, ascii_alphabetic| {
+            let byte = ascii_alphabetic as u8;
+            let index = if (65..=90).contains(&byte) {
+                byte - 65
+            } else {
+                byte - 97
+            };
+            acc + ALPHABET_SCORES[index as usize]
+        })
 }
