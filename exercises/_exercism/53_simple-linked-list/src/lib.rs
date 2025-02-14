93e1e173913f7893b6f67f8980d977afb4a33aac
Hans Escobar <hans-escobar@proton.me>
2025-02-14 11:14:59 -0500

    feat: exercise completed

--- exercises/_exercism/53_simple-linked-list/src/lib.rs
+++ exercises/_exercism/53_simple-linked-list/src/lib.rs
@@ -129,30 +129,31 @@ impl<T> SimpleLinkedList<T> {
     }
 
     #[must_use]
-    pub fn rev(self) -> SimpleLinkedList<T> {
-        todo!()
+    pub fn rev(mut self) -> SimpleLinkedList<T> {
+        let mut stack: Vec<T> = Vec::new();
+        while let Some(element) = self.pop() {
+            stack.push(element)
+        }
+        stack.into_iter().collect()
     }
 }
 
 impl<T> FromIterator<T> for SimpleLinkedList<T> {
-    fn from_iter<I: IntoIterator<Item = T>>(_iter: I) -> Self {
-        todo!()
+    fn from_iter<I: IntoIterator<Item = T>>(iter: I) -> Self {
+        let mut list = Self::new();
+        for element in iter {
+            list.push(element)
+        }
+        list
     }
 }
 
-// In general, it would be preferable to implement IntoIterator for SimpleLinkedList<T>
-// instead of implementing an explicit conversion to a vector. This is because, together,
-// FromIterator and IntoIterator enable conversion between arbitrary collections.
-//
-// The reason this exercise's API includes an explicit conversion to Vec<T> instead
-// of IntoIterator is that implementing that interface is fairly complicated, and
-// demands more of the student than we expect at this point in the track.
-//
-// Please note that the "front" of the linked list should correspond to the "back"
-// of the vector as far as the tests are concerned.
-
 impl<T> From<SimpleLinkedList<T>> for Vec<T> {
-    fn from(mut _linked_list: SimpleLinkedList<T>) -> Vec<T> {
-        todo!()
+    fn from(mut linked_list: SimpleLinkedList<T>) -> Vec<T> {
+        let mut stack: Vec<T> = Vec::new();
+        while let Some(element) = linked_list.pop() {
+            stack.push(element)
+        }
+        stack.into_iter().rev().collect()
     }
 }
