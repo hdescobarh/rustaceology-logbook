aaa3d52186dc1adbb904f7d24c7dc045f7a911cc
Hans Escobar <hans-escobar@proton.me>
2025-02-15 11:09:55 -0500

    feat: added iterator that generates the sequences of movements

--- exercises/_exercism/54_spiral-matrix/src/lib.rs
+++ exercises/_exercism/54_spiral-matrix/src/lib.rs
@@ -11,6 +11,93 @@
 // (a) f(m-1) = mn
 // (b) f(n) = mn
 // Then, the strategy is to implement a generator of the pattern
+#[derive(Copy, Clone, Debug, PartialEq)]
+enum Direction {
+    Init,
+    Right(usize),
+    Down(usize),
+    Left(usize),
+    Up(usize),
+}
+
+impl Direction {
+    fn next(&self, steps: usize) -> Self {
+        match self {
+            Direction::Init => Direction::Right(steps),
+            Direction::Right(_) => Direction::Down(steps),
+            Direction::Down(_) => Direction::Left(steps),
+            Direction::Left(_) => Direction::Up(steps),
+            Direction::Up(_) => Direction::Right(steps),
+        }
+    }
+}
+struct Movement {
+    remaining_rows: usize,
+    remaining_cols: usize,
+    cycle: usize,
+    direction: Direction,
+}
+
+impl Movement {
+    pub fn new(rows: usize, cols: usize) -> Self {
+        Self {
+            remaining_rows: rows - 1,
+            remaining_cols: cols,
+            cycle: 0,
+            direction: Direction::Init,
+        }
+    }
+}
+
+impl Iterator for Movement {
+    type Item = Direction;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.cycle % 2 == 0 {
+            self.direction = self.direction.next(self.remaining_cols);
+            self.remaining_cols = self.remaining_cols.checked_sub(1)?;
+        } else {
+            self.direction = self.direction.next(self.remaining_rows);
+            self.remaining_rows = self.remaining_rows.checked_sub(1)?;
+        }
+        self.cycle += 1;
+        Some(self.direction)
+    }
+}
 pub fn spiral_matrix(size: u32) -> Vec<Vec<u32>> {
     todo!("Function that returns the spiral matrix of square size {size}");
+}
+
+#[cfg(test)]
+mod test {
+    use crate::*;
+
+    #[test]
+    fn directions_cols_smallest() {
+        let (rows, cols) = (3, 4);
+        let moves: Vec<Direction> = Movement::new(rows, cols).take(rows * cols).collect();
+        let expect = vec![
+            Direction::Right(4),
+            Direction::Down(2),
+            Direction::Left(3),
+            Direction::Up(1),
+            Direction::Right(2),
+        ];
+        assert_eq!(moves, expect)
+    }
+
+    #[test]
+    fn directions_rows_smallest() {
+        let (rows, cols) = (4, 3);
+        let moves: Vec<Direction> = Movement::new(rows, cols).take(rows * cols).collect();
+        let expect = vec![
+            Direction::Right(3),
+            Direction::Down(3),
+            Direction::Left(2),
+            Direction::Up(2),
+            Direction::Right(1),
+            Direction::Down(1),
+        ];
+        assert_eq!(moves, expect)
+    }
 }
