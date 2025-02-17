8a435dd50ccce64b8e38a4ac43ffaec706c3e967
Hans Escobar <hans-escobar@proton.me>
2025-02-16 22:41:31 -0500

    feat: implemented preliminar solution

--- exercises/_exercism/55_tournament/src/lib.rs
+++ exercises/_exercism/55_tournament/src/lib.rs
@@ -1,13 +1,17 @@
-use std::{collections::HashMap, fmt::Display};
+use std::cmp::Reverse;
+use std::collections::BTreeMap;
+use std::fmt::Display;
 const COLUMN_DELIMITER: char = ';';
 const WIN_SCORE: u16 = 3;
 const DRAW_SCORE: u16 = 1;
 const LOSS_SCORE: u16 = 0;
 
 pub fn tally(match_results: &str) -> String {
-    todo!(
-        "Given the result of the played matches '{match_results}' return a properly formatted tally table string."
-    );
+    let mut summary = Summary::new();
+    for line in match_results.lines() {
+        summary.read_line(line);
+    }
+    summary.to_string()
 }
 
 enum MatchResult {
@@ -31,7 +35,7 @@ impl TryFrom<&str> for MatchResult {
 }
 
 impl MatchResult {
-    fn as_rival_result(&self) -> Self {
+    fn as_rival_result(&self) -> MatchResult {
         match &self {
             MatchResult::Loss => Self::Win,
             MatchResult::Draw => Self::Draw,
@@ -40,6 +44,7 @@ impl MatchResult {
     }
 }
 
+#[derive(Clone)]
 struct TeamStats {
     team: String,
     games: u16,
@@ -61,6 +66,11 @@ impl TeamStats {
         }
     }
 
+    fn with_match(mut self, result: &MatchResult) -> Self {
+        self.add_match_result(result);
+        self
+    }
+
     fn add_match_result(&mut self, result: &MatchResult) {
         self.games += 1;
         match result {
@@ -80,12 +90,75 @@ impl TeamStats {
     }
 }
 
-impl Display for TeamStats {
+struct Summary<'a> {
+    data: BTreeMap<&'a str, TeamStats>,
+}
+
+impl<'a> Summary<'a> {
+    fn new() -> Self {
+        Self {
+            data: BTreeMap::new(),
+        }
+    }
+
+    fn read_line(&mut self, line: &'a str) {
+        let mut match_result = None::<MatchResult>;
+        for (col, value) in line.split(COLUMN_DELIMITER).rev().enumerate() {
+            let team_result = match col {
+                0 => {
+                    match_result = MatchResult::try_from(value).ok();
+                    continue;
+                }
+                1 => &match_result.as_ref().unwrap().as_rival_result(),
+                2 => match_result.as_ref().unwrap(),
+                _ => panic!("Wrong format. Expects three columns"),
+            };
+            self.data
+                .entry(value)
+                .and_modify(|team| team.add_match_result(team_result))
+                .or_insert(TeamStats::new(value).with_match(team_result));
+        }
+    }
+}
+
+impl Display for Summary<'_> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
+        let mut output: Vec<&TeamStats> = self.data.values().collect();
+        output.sort_by_key(|team| Reverse(team.points));
+        let w1 = 31;
+        let w2 = 2;
+        writeln!(
             f,
-            "{:31}| {:2} | {:2} | {:2} | {:2} | {:2}",
-            self.team, self.games, self.won, self.tied, self.lost, self.points
-        )
+            "{:<w1$}| {:>w2$} | {:>w2$} | {:>w2$} | {:>w2$} | {:>w2$}",
+            "Team", "MP", "W", "D", "L", "P"
+        )?;
+
+        for stats in &output[..output.len() - 1] {
+            writeln!(
+                f,
+                "{:<w1$}| {:>w2$} | {:>w2$} | {:>w2$} | {:>w2$} | {:>w2$}",
+                stats.team, stats.games, stats.won, stats.tied, stats.lost, stats.points
+            )?;
+        }
+
+        if let Some(stats) = output.last() {
+            write!(
+                f,
+                "{:<w1$}| {:>w2$} | {:>w2$} | {:>w2$} | {:>w2$} | {:>w2$}",
+                stats.team, stats.games, stats.won, stats.tied, stats.lost, stats.points
+            )?;
+        }
+
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod test {
+
+    #[test]
+    fn prueba() {
+        let input: &[&str] = &["Allegoric Alaskans;Blithering Badgers;win"];
+        let input = input.join("\n");
     }
 }
