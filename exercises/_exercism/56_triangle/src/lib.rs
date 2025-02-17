cd7111ca1f689373620b80917400ea751878cfdf
Hans Escobar <hans-escobar@proton.me>
2025-02-17 04:51:46 -0500

    feat: exercise solved for integers

--- exercises/_exercism/56_triangle/src/lib.rs
+++ exercises/_exercism/56_triangle/src/lib.rs
@@ -1,19 +1,40 @@
-pub struct Triangle;
+use std::ops::Add;
 
-impl Triangle {
-    pub fn build(sides: [u64; 3]) -> Option<Triangle> {
-        todo!("Construct new Triangle from following sides: {sides:?}. Return None if the sides are invalid.");
+pub struct Triangle<T>
+where
+    T: Copy + Add<Output = T> + PartialOrd + PartialEq,
+{
+    a: T,
+    b: T,
+    c: T,
+}
+
+impl<T> Triangle<T>
+where
+    T: Copy + Add<Output = T> + PartialOrd + PartialEq,
+{
+    pub fn build(sides: [T; 3]) -> Option<Triangle<T>> {
+        let [a, b, c] = sides;
+        if a + b < c || b + c < a || c + a < b || Self::any_zero(a, b, c) {
+            return None;
+        }
+        Some(Self { a, b, c })
+    }
+
+    fn any_zero(a: T, b: T, c: T) -> bool {
+        let sum = a + b + c;
+        sum + a == sum || sum + b == sum || sum + c == sum
     }
 
     pub fn is_equilateral(&self) -> bool {
-        todo!("Determine if the Triangle is equilateral.");
+        self.a == self.b && self.a == self.c && self.c == self.b
     }
 
     pub fn is_scalene(&self) -> bool {
-        todo!("Determine if the Triangle is scalene.");
+        self.a != self.b && self.a != self.c && self.c != self.b
     }
 
     pub fn is_isosceles(&self) -> bool {
-        todo!("Determine if the Triangle is isosceles.");
+        self.a == self.b || self.a == self.c || self.c == self.b
     }
 }
