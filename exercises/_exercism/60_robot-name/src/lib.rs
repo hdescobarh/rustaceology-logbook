59bd74ace442bd88aca2037d78130ae02c5db291
Hans Escobar <hans-escobar@proton.me>
2025-02-19 17:05:56 -0500

    feat: added global thread-safe state with LazyLock and Mutex

--- exercises/_exercism/60_robot-name/src/lib.rs
+++ exercises/_exercism/60_robot-name/src/lib.rs
@@ -1,7 +1,13 @@
 // 26^2 * 10^3 = 676_000 possible sequences, each one totally characterized by
 // their number. It is enough to get a single random int in that range.
 use rand::Rng;
-use std::collections::HashSet;
+use std::{
+    collections::HashSet,
+    sync::{LazyLock, Mutex},
+};
+
+static ID_REGISTRY: LazyLock<Mutex<HashSet<u32>>> = LazyLock::new(|| Mutex::new(HashSet::new()));
+
 pub struct Robot {
     id: u32,
     name: String,
@@ -14,10 +20,9 @@ impl Robot {
         }
         let mut rng = rand::thread_rng();
         let mut id = rng.gen_range(0_u32..676_000);
-        while excluded.contains(&id) {
+        while !excluded.insert(id) {
             id = rng.gen_range(0_u32..676_000);
         }
-        excluded.insert(id);
         let digits = id % 1000;
         let letter_1 = char::from_u32(id / 1000 % 26 + 65).unwrap();
         let letter_2 = char::from_u32(id / 26000 + 65).unwrap();
@@ -25,7 +30,7 @@ impl Robot {
     }
 
     pub fn new() -> Self {
-        let (id, name) = Self::generate_id(&mut HashSet::new());
+        let (id, name) = Self::generate_id(&mut ID_REGISTRY.lock().unwrap());
         Self { id, name }
     }
 
@@ -34,6 +39,9 @@ impl Robot {
     }
 
     pub fn reset_name(&mut self) {
-        (self.id, self.name) = Self::generate_id(&mut HashSet::from([self.id]));
+        let old_id = self.id;
+        let registry = &mut ID_REGISTRY.lock().unwrap();
+        (self.id, self.name) = Self::generate_id(registry);
+        registry.remove(&old_id);
     }
 }
