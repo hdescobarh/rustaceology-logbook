c9d4967851bcf4654f5e9ac91ff023c86b57c4c7
Hans Escobar <hans-escobar@proton.me>
2025-02-20 20:14:03 -0500

    feat: added general structure for the solution

--- exercises/_exercism/61_protein-translation/src/lib.rs
+++ exercises/_exercism/61_protein-translation/src/lib.rs
@@ -1,3 +1,49 @@
 pub fn translate(rna: &str) -> Option<Vec<&str>> {
-    todo!("Return a list of protein names that correspond to the '{rna}' RNA string or None if the RNA string is invalid");
+    rna.as_bytes()
+        .chunks(3)
+        .map_while(|code| {
+            Codon::new(code)
+                .map(|codon| codon.translate_from_std_table().map(|aa| aa.into()))
+                .transpose()
+        })
+        .collect::<Result<Vec<&str>, Error>>()
+        .ok()
+}
+
+enum Error {
+    InvalidNucleotideCode,
+    InvalidSequenceLenght,
+}
+
+enum RnaNucleotide {
+    A,
+    G,
+    C,
+    U,
+}
+
+impl TryFrom<u8> for RnaNucleotide {
+    type Error = Error;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        todo!()
+    }
+}
+struct Codon(Vec<RnaNucleotide>);
+
+impl Codon {
+    fn new(code: &[u8]) -> Result<Self, Error> {
+        todo!()
+    }
+
+    fn translate_from_std_table(&self) -> Option<AminoAcid> {
+        todo!()
+    }
+}
+enum AminoAcid {}
+
+impl From<AminoAcid> for &str {
+    fn from(value: AminoAcid) -> Self {
+        todo!()
+    }
 }
