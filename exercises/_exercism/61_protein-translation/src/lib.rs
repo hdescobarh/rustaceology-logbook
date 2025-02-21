af745bfa6e3d5f4b32f965abed38503413788be8
Hans Escobar <hans-escobar@proton.me>
2025-02-20 22:11:02 -0500

    feat: added Codon creator and translation table. Exercise solved

--- exercises/_exercism/61_protein-translation/src/lib.rs
+++ exercises/_exercism/61_protein-translation/src/lib.rs
@@ -41,12 +41,43 @@ impl TryFrom<u8> for RnaNucleotide {
 struct Codon(Vec<RnaNucleotide>);
 
 impl Codon {
-    fn new(code: &[u8]) -> Result<Self, Error> {
-        todo!()
+    fn new(codes: &[u8]) -> Result<Self, Error> {
+        if codes.len() != 3 {
+            return Err(Error::InvalidSequenceLength);
+        }
+        Ok(Self(
+            codes
+                .iter()
+                .map(|&b| RnaNucleotide::try_from(b))
+                .collect::<Result<Vec<RnaNucleotide>, Error>>()?,
+        ))
     }
 
     fn translate_from_std_table(&self) -> Option<AminoAcid> {
-        todo!()
+        let aa = match (&self.0[0], &self.0[1], &self.0[2]) {
+            (G, C, _) => Ala,
+            (C, G, _) | (A, G, A) | (A, G, G) => Arg,
+            (A, A, U) | (A, A, C) => Asn,
+            (G, A, U) | (G, A, C) => Asp,
+            (U, G, U) | (U, G, C) => Cys,
+            (C, A, A) | (C, A, G) => Gln,
+            (G, A, A) | (G, A, G) => Glu,
+            (G, G, _) => Gly,
+            (C, A, U) | (C, A, C) => His,
+            (A, U, U) | (A, U, C) | (A, U, A) => Ile,
+            (C, U, _) | (U, U, A) | (U, U, G) => Leu,
+            (A, A, A) | (A, A, G) => Lys,
+            (A, U, G) => Met,
+            (U, U, U) | (U, U, C) => Phe,
+            (C, C, _) => Pro,
+            (U, C, _) | (A, G, U) | (A, G, C) => Ser,
+            (A, C, _) => Thr,
+            (U, G, G) => Trp,
+            (U, A, U) | (U, A, C) => Tyr,
+            (G, U, _) => Val,
+            (U, A, A) | (U, G, A) | (U, A, G) => return None,
+        };
+        Some(aa)
     }
 }
 enum AminoAcid {
