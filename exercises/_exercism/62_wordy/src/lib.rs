5489997f50ad118fce66448df39afe592268e3a9
Hans Escobar <hans-escobar@proton.me>
2025-02-22 23:57:42 -0500

    feat: added solution general strategy

--- exercises/_exercism/62_wordy/src/lib.rs
+++ exercises/_exercism/62_wordy/src/lib.rs
@@ -1,3 +1,59 @@
+use regex_lite::Regex;
+use std::error::Error;
+
 pub fn answer(command: &str) -> Option<i32> {
-    todo!("Return the result of the command '{command}' or None, if the command is invalid.");
+    Parser::read(command)
+        .and_then(|operations| {
+            operations
+                .into_iter()
+                .try_fold(Calculator::new(), |calculator, operation| {
+                    calculator.read(operation)
+                })
+                .and_then(|calculator| calculator.finish())
+        })
+        .ok()
+}
+
+struct Calculator {
+    num1: Option<i32>,
+    num2: Option<i32>,
+    operation: Option<Operation>,
+    total: Option<i32>,
+}
+
+impl Calculator {
+    fn new() -> Self {
+        Self {
+            num1: None,
+            num2: None,
+            operation: None,
+            total: None,
+        }
+    }
+
+    fn read(self, operation: Operation) -> Result<Self, Box<dyn Error>> {
+        todo!()
+    }
+
+    fn finish(self) -> Result<i32, Box<dyn Error>> {
+        todo!()
+    }
+}
+
+enum Operation {
+    Init(i32),
+    Add(i32),
+    Sub(i32),
+    Prod(i32),
+    Div(i32),
+    Pow(u32),
+    Total,
+}
+
+struct Parser;
+
+impl Parser {
+    fn read(command: &str) -> Result<Vec<Operation>, Box<dyn Error>> {
+        todo!()
+    }
 }
