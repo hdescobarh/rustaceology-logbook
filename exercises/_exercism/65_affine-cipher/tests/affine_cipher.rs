b7886fd4518c299403b7b3898303b8b1b0cf5ad4
Hans Escobar <hans-escobar@proton.me>
2025-03-04 23:22:28 -0500

    test: enabled all encoding tests

--- exercises/_exercism/65_affine-cipher/tests/affine_cipher.rs
+++ exercises/_exercism/65_affine-cipher/tests/affine_cipher.rs
@@ -12,7 +12,6 @@ fn encode_yes() {
 }
 
 #[test]
-#[ignore]
 fn encode_no() {
     let phrase = "no";
     let (a, b) = (15, 18);
@@ -22,7 +21,6 @@ fn encode_no() {
 }
 
 #[test]
-#[ignore]
 fn encode_omg() {
     let phrase = "OMG";
     let (a, b) = (21, 3);
@@ -32,7 +30,6 @@ fn encode_omg() {
 }
 
 #[test]
-#[ignore]
 fn encode_o_m_g() {
     let phrase = "O M G";
     let (a, b) = (25, 47);
@@ -42,7 +39,6 @@ fn encode_o_m_g() {
 }
 
 #[test]
-#[ignore]
 fn encode_mindblowingly() {
     let phrase = "mindblowingly";
     let (a, b) = (11, 15);
@@ -52,7 +48,6 @@ fn encode_mindblowingly() {
 }
 
 #[test]
-#[ignore]
 fn encode_numbers() {
     let phrase = "Testing,1 2 3, testing.";
     let (a, b) = (3, 4);
@@ -62,7 +57,6 @@ fn encode_numbers() {
 }
 
 #[test]
-#[ignore]
 fn encode_deep_thought() {
     let phrase = "Truth is fiction.";
     let (a, b) = (5, 17);
@@ -72,7 +66,6 @@ fn encode_deep_thought() {
 }
 
 #[test]
-#[ignore]
 fn encode_all_the_letters() {
     let phrase = "The quick brown fox jumps over the lazy dog.";
     let (a, b) = (17, 33);
@@ -82,7 +75,6 @@ fn encode_all_the_letters() {
 }
 
 #[test]
-#[ignore]
 fn encode_with_a_not_coprime_to_m() {
     let phrase = "This is a test.";
     let (a, b) = (6, 17);
