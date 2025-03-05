2350d7d0016b4523f7eaad285461ce068e30d299
Hans Escobar <hans-escobar@proton.me>
2025-03-05 00:43:51 -0500

    refactor: change integer types and removed unused methods

--- exercises/_exercism/65_affine-cipher/src/lib.rs
+++ exercises/_exercism/65_affine-cipher/src/lib.rs
@@ -1,9 +1,9 @@
 #[derive(Debug, Eq, PartialEq)]
 pub enum AffineCipherError {
-    NotCoprime(u32),
+    NotCoprime(u8),
 }
 
-pub fn encode(plaintext: &str, a: u32, b: u32) -> Result<String, AffineCipherError> {
+pub fn encode(plaintext: &str, a: u8, b: u8) -> Result<String, AffineCipherError> {
     AffineCipher::new(a, b).map(|cipher| {
         plaintext
             .chars()
@@ -19,85 +19,40 @@ pub fn encode(plaintext: &str, a: u32, b: u32) -> Result<String, AffineCipherErr
     })
 }
 
-pub fn decode(ciphertext: &str, a: u32, b: u32) -> Result<String, AffineCipherError> {
+pub fn decode(ciphertext: &str, a: u8, b: u8) -> Result<String, AffineCipherError> {
     let cipher = AffineCipher::new(a, b)?;
     todo!("Decode {ciphertext} with the key ({a}, {b})");
 }
 
-struct AffineCipher(u32, u32);
+struct AffineCipher(u16, u16);
 
 impl AffineCipher {
-    pub fn new(a: u32, b: u32) -> Result<Self, AffineCipherError> {
+    pub fn new(a: u8, b: u8) -> Result<Self, AffineCipherError> {
         let (max, min) = if a >= 26 { (a, 26) } else { (26, a) };
-        match Self::extended_euclidean(max as i64, min as i64) {
+        match Self::extended_euclidean(max as i16, min as i16) {
             (1, _, _) => (),
             _ => return Err(AffineCipherError::NotCoprime(a)),
         }
-        Ok(AffineCipher(a, b))
+        Ok(AffineCipher(a as u16, b as u16))
     }
 
     fn plain_to_cypher(&self, letter: char) -> Option<char> {
         let letter_index = match letter {
-            'A'..='Z' => (letter as u8 - b'A') as u32,
-            'a'..='z' => (letter as u8 - b'a') as u32,
+            'A'..='Z' => letter as u8 - b'A',
+            'a'..='z' => letter as u8 - b'a',
             '0'..='9' => return Some(letter),
             _ => return None,
         };
-        let cypher_letter = b'a' + ((self.0 * letter_index + self.1) % 26) as u8;
+        let cypher_letter = b'a' + ((self.0 * letter_index as u16 + self.1) % 26) as u8;
         Some(cypher_letter.into())
     }
 
-    fn greatest_common_divisor(a: u32, b: u32) -> u32 {
-        let (max, min) = if a >= b { (a, b) } else { (b, a) };
-        if min == 0 {
-            return max;
-        }
-        Self::greatest_common_divisor(min, max % min)
-    }
-
     /// Computes the gcd and the Bézout's identity coefficients
-    fn extended_euclidean(a: i64, b: i64) -> (i64, i64, i64) {
+    fn extended_euclidean(a: i16, b: i16) -> (i16, i16, i16) {
         if b == 0 {
             return (a, 1, 0);
         }
         let (gcd, x, y) = Self::extended_euclidean(b, a % b);
         (gcd, y, x - (a / b) * y)
-    }
-}
-#[cfg(test)]
-mod test {
-    use crate::*;
-
-    #[test]
-    fn gcd_not_coprime() {
-        let cases = [
-            (2, 8, 2),
-            (3, 27, 3),
-            (64, 1024, 64),
-            (1071, 462, 21),
-            (1236, 2460, 12),
-            (5275, 7425, 25),
-            (8120, 9240, 280),
-        ];
-        for (a, b, expected) in cases {
-            assert_eq!(AffineCipher::greatest_common_divisor(a, b), expected);
-            assert_eq!(AffineCipher::greatest_common_divisor(b, a), expected);
-        }
-    }
-
-    #[test]
-    fn gcd_coprime() {
-        let cases = [
-            (2, 3, 1),
-            (1012, 1013, 1),
-            (2021, 3037, 1),
-            (4096, 6561, 1),
-            (5003, 7001, 1),
-            (8125, 9007, 1),
-        ];
-        for (a, b, expected) in cases {
-            assert_eq!(AffineCipher::greatest_common_divisor(a, b), expected);
-            assert_eq!(AffineCipher::greatest_common_divisor(b, a), expected);
-        }
     }
 }
