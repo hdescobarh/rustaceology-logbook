907ffc343173f10d1f48ffcf1c9efcb7cf76ab65
Hans Escobar <hans-escobar@proton.me>
2025-03-07 03:40:25 -0500

    refactor: remove unnecesary comments

--- exercises/_exercism/67_crypto-square/src/lib.rs
+++ exercises/_exercism/67_crypto-square/src/lib.rs
@@ -21,13 +21,6 @@ impl Cipher {
         Self { rows, cols, normal }
     }
 
-    // From the constraints:
-    // - Option 1: c_1 = round(sqrt(n)), with r = c_1
-    // - Option 2: c_2 = ceil(0.5 + sqrt(0.25 + n)), with r = c_1 - 1
-    // Since min(c_1, c_2) = c_1, the choice depends on whether c_1^2 >= n.
-    //
-    // I haven't found a formal proof, but I suspect that c_2 = c_1 + 1.
-    // I verified this holds for the first 10^9 integers.
     fn determine_dimensions(normal_length: f64) -> (usize, usize) {
         let check_sqrt = normal_length.sqrt().round() as usize;
         if check_sqrt.pow(2) >= normal_length as usize {
