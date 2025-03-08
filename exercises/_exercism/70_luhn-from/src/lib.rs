317cc4345a9fc11564b2ae47fd7102860a404db2
Hans Escobar <hans-escobar@proton.me>
2025-03-07 20:38:42 -0500

    feat: implement From<T> trait and validates the string

--- exercises/_exercism/70_luhn-from/src/lib.rs
+++ exercises/_exercism/70_luhn-from/src/lib.rs
@@ -1,18 +1,42 @@
-pub struct Luhn;
+pub struct Luhn(String);
 
 impl Luhn {
     pub fn is_valid(&self) -> bool {
-        todo!("Determine if the current Luhn struct contains a valid credit card number.");
+        self.check_chars_rev()
+            .try_fold((0, false, 0), |mut acc, maybe_digit| {
+                maybe_digit.map(|digit| {
+                    if acc.1 {
+                        acc.0 += if digit < 5 { digit * 2 } else { digit * 2 - 9 };
+                    } else {
+                        acc.0 += digit
+                    }
+                    acc.1 = !acc.1;
+                    acc.2 += 1;
+                    acc
+                })
+            })
+            .is_ok_and(|(sum, _, length)| sum.rem_euclid(10) == 0 && length > 1)
+    }
+
+    fn check_chars_rev(&self) -> impl Iterator<Item = Result<u32, ()>> {
+        self.0
+            .chars()
+            .filter_map(|c| {
+                if c.is_ascii_whitespace() {
+                    None
+                } else {
+                    Some(c.to_digit(10).ok_or(()))
+                }
+            })
+            .rev()
     }
 }
 
-/// Here is the example of how the From trait could be implemented
-/// for the &str type. Naturally, you can implement this trait
-/// by hand for every other type presented in the test suite,
-/// but your solution will fail if a new type is presented.
-/// Perhaps there exists a better solution for this problem?
-impl From<&str> for Luhn {
-    fn from(input: &str) -> Self {
-        todo!("From the given input '{input}' create a new Luhn struct.");
+impl<T> From<T> for Luhn
+where
+    T: ToString,
+{
+    fn from(value: T) -> Self {
+        Self(value.to_string())
     }
 }
