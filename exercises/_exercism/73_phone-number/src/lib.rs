96c4c96d0100dd8c79ba7a49ede6ca1d1f323794
Hans Escobar <hans-escobar@proton.me>
2025-03-09 20:06:35 -0500

    refactor: rewrite in an imperative style

--- exercises/_exercism/73_phone-number/src/lib.rs
+++ exercises/_exercism/73_phone-number/src/lib.rs
@@ -1,33 +1,20 @@
 const ALLOWED_NON_DIGITS: [char; 6] = [' ', '(', ')', '+', '-', '.'];
 pub fn number(user_number: &str) -> Option<String> {
-    user_number
-        .chars()
-        .filter_map(|c| {
-            if c.is_ascii_digit() {
-                Some(Ok(c))
-            } else if ALLOWED_NON_DIGITS.contains(&c) {
-                None
-            } else {
-                Some(Err(()))
-            }
-        })
-        .try_fold(String::with_capacity(10), |mut acc, maybe_digit| {
-            maybe_digit.and_then(|digit| {
-                match digit {
-                    '1' if acc.is_empty() => (),
-                    '0' if acc.is_empty() => return Err(()),
-                    '0'..='1' if acc.len() == 3 => return Err(()),
-                    _ => acc.push(digit),
-                };
-                Ok(acc)
-            })
-        })
-        .map(|number| {
-            if number.len() == 10 {
-                Some(number)
-            } else {
-                None
-            }
-        })
-        .unwrap_or_default()
+    let mut acc = String::with_capacity(10);
+    for digit in user_number.chars() {
+        // Check it is a allowed character
+        if ALLOWED_NON_DIGITS.contains(&digit) {
+            continue;
+        } else if !digit.is_ascii_digit() {
+            return None;
+        };
+        // Check digit is valid
+        match digit {
+            '0' if acc.is_empty() => return None,
+            '1' if acc.is_empty() => (),
+            '0'..='1' if acc.len() == 3 => return None,
+            _ => acc.push(digit),
+        }
+    }
+    if acc.len() == 10 { Some(acc) } else { None }
 }
