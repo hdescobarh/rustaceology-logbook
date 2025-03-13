e2885e36235224b95d32961fa18339cc9063da6f
Hans Escobar <hans-escobar@proton.me>
2025-03-13 02:05:39 -0500

    feat: implement encoding

--- exercises/_exercism/74_rail-fence-cipher/src/lib.rs
+++ exercises/_exercism/74_rail-fence-cipher/src/lib.rs
@@ -1,12 +1,31 @@
-pub struct RailFence;
+pub struct RailFence {
+    rails: usize,
+    period: usize,
+}
 
 impl RailFence {
     pub fn new(rails: u32) -> RailFence {
-        todo!("Construct a new fence with {rails} rails")
+        Self {
+            rails: rails as usize,
+            period: (2 * rails - 2) as usize,
+        }
     }
 
     pub fn encode(&self, text: &str) -> String {
-        todo!("Encode this text: {text}")
+        text.char_indices()
+            .fold(
+                vec![String::new(); self.rails],
+                |mut acc, (index, letter)| {
+                    let cycle_position = index % self.period;
+                    let row = match cycle_position.cmp(&self.rails) {
+                        std::cmp::Ordering::Less => cycle_position,
+                        _ => self.period - cycle_position,
+                    };
+                    acc[row].push(letter);
+                    acc
+                },
+            )
+            .join("")
     }
 
     pub fn decode(&self, cipher: &str) -> String {
