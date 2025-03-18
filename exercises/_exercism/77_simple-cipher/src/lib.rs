de09a541b84d514d472d47e7e2b5337b9571dd6c
Hans Escobar <hans-escobar@proton.me>
2025-03-17 21:58:19 -0500

    feat: implement encode

--- exercises/_exercism/77_simple-cipher/src/lib.rs
+++ exercises/_exercism/77_simple-cipher/src/lib.rs
@@ -1,5 +1,16 @@
-pub fn encode(key: &str, s: &str) -> Option<String> {
-    todo!("Use {key} to encode {s} using shift cipher")
+pub fn encode(key: &str, plain: &str) -> Option<String> {
+    plain
+        .chars()
+        .zip(key.chars())
+        .map(|(c, shift)| {
+            if !shift.is_ascii_lowercase() {
+                return Err(());
+            };
+            Ok(((c.to_ascii_lowercase() as u8 + shift as u8 - 2 * b'a') % 26 + b'a') as char)
+        })
+        .collect::<Result<String, ()>>()
+        .map(|out| if out.is_empty() { None } else { Some(out) })
+        .unwrap_or_default()
 }
 
 pub fn decode(key: &str, s: &str) -> Option<String> {
