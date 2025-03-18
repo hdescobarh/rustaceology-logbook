45693873ed9507832621669fc1b6a796547caaa6
Hans Escobar <hans-escobar@proton.me>
2025-03-17 23:12:33 -0500

    refactor: separate parsing from conversion steps

--- exercises/_exercism/77_simple-cipher/src/lib.rs
+++ exercises/_exercism/77_simple-cipher/src/lib.rs
@@ -1,31 +1,27 @@
-pub fn encode(key: &str, plain: &str) -> Option<String> {
-    plain
-        .chars()
+fn parser<F: Fn(char, char) -> u8>(key: &str, text: &str, op: F) -> Option<String> {
+    text.chars()
         .zip(key.chars().cycle())
         .map(|(c, shift)| {
             if !shift.is_ascii_lowercase() || !c.is_alphanumeric() {
                 return Err(());
             };
-            Ok(((c.to_ascii_lowercase() as u8 + shift as u8 - 2 * b'a') % 26 + b'a') as char)
+            Ok(((op)(c, shift) % 26 + b'a') as char)
         })
         .collect::<Result<String, ()>>()
         .map(|out| if out.is_empty() { None } else { Some(out) })
         .unwrap_or_default()
 }
 
+pub fn encode(key: &str, plain: &str) -> Option<String> {
+    parser(key, plain, |c, shift| {
+        c.to_ascii_lowercase() as u8 + shift as u8 - 2 * b'a'
+    })
+}
+
 pub fn decode(key: &str, cipher: &str) -> Option<String> {
-    cipher
-        .chars()
-        .zip(key.chars().cycle())
-        .map(|(c, shift)| {
-            if !shift.is_ascii_lowercase() || !c.is_alphanumeric() {
-                return Err(());
-            };
-            Ok(((c.to_ascii_lowercase() as u8 + 26 - shift as u8) % 26 + b'a') as char)
-        })
-        .collect::<Result<String, ()>>()
-        .map(|out| if out.is_empty() { None } else { Some(out) })
-        .unwrap_or_default()
+    parser(key, cipher, |c, shift| {
+        c.to_ascii_lowercase() as u8 + 26 - shift as u8
+    })
 }
 
 pub fn encode_random(s: &str) -> (String, String) {
