2669b077c2a0f2fafb97743c53d11f70343d8740
Hans Escobar <hans-escobar@proton.me>
2025-03-19 17:48:44 -0500

    feat: implement knapsack discrete without repeats

--- exercises/_exercism/80_knapsack/src/lib.rs
+++ exercises/_exercism/80_knapsack/src/lib.rs
@@ -1,9 +1,26 @@
+use std::collections::HashMap;
+
 #[derive(Debug)]
 pub struct Item {
     pub weight: u32,
     pub value: u32,
 }
 
+// Let w ∈ [0, max_weight], i ∈ [0, items.len()], maximum_value = value(w, i).
+// Then, maximum_value = max{value(w - w_i, i-1) + v_i, value(w, i-1)}
 pub fn maximum_value(max_weight: u32, items: &[Item]) -> u32 {
-    todo!("calculate the maximum value achievable with the given {items:?} and {max_weight}");
+    let mut value: HashMap<(u32, usize), u32> = HashMap::new();
+    for i in 1..=items.len() {
+        for w in 1..=max_weight {
+            value.insert((w, i), *value.get(&(w, i - 1)).unwrap_or(&0));
+            if items[i - 1].weight <= w {
+                let val =
+                    value.get(&(w - items[i - 1].weight, i - 1)).unwrap_or(&0) + items[i - 1].value;
+                if value[&(w, i)] < val {
+                    value.insert((w, i), val);
+                }
+            }
+        }
+    }
+    *value.get(&(max_weight, items.len())).unwrap_or(&0)
 }
