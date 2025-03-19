21f5708922b5ba3d1edd77f66ca086f90ff99423
Hans Escobar <hans-escobar@proton.me>
2025-03-19 15:19:44 -0500

    feat: implement actions

--- exercises/_exercism/79_secret-handshake/src/lib.rs
+++ exercises/_exercism/79_secret-handshake/src/lib.rs
@@ -1,3 +1,14 @@
 pub fn actions(n: u8) -> Vec<&'static str> {
-    todo!("What is the secret handshake for {n}?")
+    let mut output = Vec::with_capacity(4);
+    for step in 0..5 {
+        match ((n >> step) & 1, step) {
+            (1, 0) => output.push("wink"),
+            (1, 1) => output.push("double blink"),
+            (1, 2) => output.push("close your eyes"),
+            (1, 3) => output.push("jump"),
+            (1, 4) => output.reverse(),
+            _ => continue,
+        };
+    }
+    output
 }
