64b83e59b624d8a4263df71682d54fa297fe5936
Hans Escobar <hans-escobar@proton.me>
2025-03-19 18:55:24 -0500

    refactor: rename variables to improve readability

--- exercises/_exercism/80_knapsack/src/lib.rs
+++ exercises/_exercism/80_knapsack/src/lib.rs
@@ -6,8 +6,6 @@ pub struct Item {
     pub value: u32,
 }
 
-// Let w ∈ [0, max_weight], i ∈ [0, items.len()], maximum_value = value(w, i).
-// Then, maximum_value = max{value(w - w_i, i-1) + v_i, value(w, i-1)}
 pub fn maximum_value(max_weight: u32, items: &[Item]) -> u32 {
     let mut value: HashMap<(u32, usize), u32> = HashMap::new();
     for i in 1..=items.len() {
@@ -15,9 +13,10 @@ pub fn maximum_value(max_weight: u32, items: &[Item]) -> u32 {
             value.insert((bag_size, i), *value.get(&(bag_size, i - 1)).unwrap_or(&0));
             let item = &items[i - 1];
             if item.weight <= bag_size {
-                let val = value.get(&(bag_size - item.weight, i - 1)).unwrap_or(&0) + item.value;
-                if value[&(bag_size, i)] < val {
-                    value.insert((bag_size, i), val);
+                let maybe_higher =
+                    value.get(&(bag_size - item.weight, i - 1)).unwrap_or(&0) + item.value;
+                if value[&(bag_size, i)] < maybe_higher {
+                    value.insert((bag_size, i), maybe_higher);
                 }
             }
         }
