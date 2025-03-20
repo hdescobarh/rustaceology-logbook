f08ab87c11e62b24d3fb935e2c38a8622afbc568
Hans Escobar <hans-escobar@proton.me>
2025-03-19 19:15:43 -0500

    refactor: replace nested conditionals for a more idiomatic aproach

--- exercises/_exercism/80_knapsack/src/lib.rs
+++ exercises/_exercism/80_knapsack/src/lib.rs
@@ -10,15 +10,13 @@ pub fn maximum_value(max_weight: u32, items: &[Item]) -> u32 {
     let mut value: HashMap<(u32, usize), u32> = HashMap::new();
     for i in 1..=items.len() {
         for bag_size in 1..=max_weight {
-            value.insert((bag_size, i), *value.get(&(bag_size, i - 1)).unwrap_or(&0));
+            let option1 = *value.get(&(bag_size, i - 1)).unwrap_or(&0);
             let item = &items[i - 1];
-            if item.weight <= bag_size {
-                let maybe_higher =
-                    value.get(&(bag_size - item.weight, i - 1)).unwrap_or(&0) + item.value;
-                if value[&(bag_size, i)] < maybe_higher {
-                    value.insert((bag_size, i), maybe_higher);
-                }
-            }
+            let option2 = bag_size
+                .checked_sub(item.weight)
+                .map(|prior| value.get(&(prior, i - 1)).unwrap_or(&0) + item.value)
+                .unwrap_or_default();
+            value.insert((bag_size, i), option1.max(option2));
         }
     }
     *value.get(&(max_weight, items.len())).unwrap_or(&0)
