77e1116f39c0565f1e6ff9bbe0f61f97538c94c9
Hans Escobar <hans-escobar@proton.me>
2025-03-20 22:26:17 -0500

    feat: implement Matrix

--- exercises/_exercism/82_matrix/src/lib.rs
+++ exercises/_exercism/82_matrix/src/lib.rs
@@ -1,17 +1,58 @@
+use std::num::ParseIntError;
+
+#[derive(Default)]
 pub struct Matrix {
-    // Implement your Matrix struct
+    rows: usize,
+    cols: usize,
+    inner: Vec<u32>, // Avoid unnecessary layers of indirection that Vec<Vec<T>> has.
 }
 
 impl Matrix {
     pub fn new(input: &str) -> Self {
-        todo!("Create new method to store the {input}")
+        Self::validate(input).unwrap_or_default()
+    }
+
+    // Not required by the exercise, but it feels odd to not check if the input is valid 😊.
+    fn validate(input: &str) -> Option<Self> {
+        let data = input
+            .lines()
+            .map(|row| {
+                row.split_whitespace()
+                    .map(|e| e.parse::<u32>())
+                    .collect::<Result<Vec<u32>, ParseIntError>>()
+            })
+            .collect::<Result<Vec<Vec<u32>>, ParseIntError>>()
+            .ok()?;
+        let cols = data.first().and_then(|first_row| {
+            if data.iter().all(|row| row.len() == first_row.len()) {
+                Some(first_row.len())
+            } else {
+                None
+            }
+        })?;
+        Some(Self {
+            rows: data.len(),
+            cols,
+            inner: data.into_iter().flatten().collect(),
+        })
     }
 
     pub fn row(&self, row_no: usize) -> Option<Vec<u32>> {
-        todo!("Return the row at {row_no} (1-indexed) or None if the number is invalid")
+        if row_no == 0 || row_no > self.rows {
+            return None;
+        }
+        Some(self.inner[self.cols * (row_no - 1)..self.cols * row_no].to_vec())
     }
 
     pub fn column(&self, col_no: usize) -> Option<Vec<u32>> {
-        todo!("Return the column at {col_no} (1-indexed) or None if the number is invalid")
+        if col_no == 0 || col_no > self.cols {
+            return None;
+        }
+        Some(
+            (col_no - 1..self.inner.len())
+                .step_by(self.cols)
+                .map(|index| self.inner[index])
+                .collect(),
+        )
     }
 }
