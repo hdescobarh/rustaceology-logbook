1e401ba8895f0abbb28b793d2856e3f0d9ecc2b6
Hans Escobar <hans-escobar@proton.me>
2025-03-30 07:42:34 -0500

    feat: implement PythagoreanTriangle with its contraints

--- exercises/_exercism/84_pythagorean-triplet/src/lib.rs
+++ exercises/_exercism/84_pythagorean-triplet/src/lib.rs
@@ -1,10 +1,16 @@
 use std::collections::HashSet;
 
-// From solving the (in)equations: 5 < small_leg < sum * (2-sqrt(2))/2
+// From solving the (in)equations: 3 < small_leg < sum * (2 - sqrt(2)) / 2
 const CONSTRAINT: f64 = 0.2928932188134524;
+//  15 - log(u32::MAX, 10) ≈ 5.36
+const TOLERANCE: f64 = 1e-5;
 
 pub fn find(sum: u32) -> HashSet<[u32; 3]> {
-    (5..(sum as f64 * CONSTRAINT).ceil() as u32)
+    // From solving the equations: If sum (perimeter) is odd, then one leg is not an integer.
+    if sum % 2 != 0 {
+        return HashSet::new();
+    }
+    (3..(sum as f64 * CONSTRAINT).ceil() as u32)
         .filter_map(|small_leg| {
             PythagoreanTriangle::new(sum, small_leg).map(|triangle| triangle.to_slice())
         })
@@ -19,24 +25,29 @@ struct PythagoreanTriangle {
 
 impl PythagoreanTriangle {
     pub fn new(perimeter: u32, small_leg: u32) -> Option<PythagoreanTriangle> {
-        let large_leg = Self::get_large_leg(small_leg, perimeter)?;
-        let hypothenuse = Self::find_integer_hypotenuse(small_leg, large_leg)?;
-        Some(Self {
-            small_leg,
-            large_leg,
-            hypothenuse,
-        })
+        // x + y + sqrt(x**2 + y**2) = p
+        let (x, p) = (small_leg as f64, perimeter as f64);
+        let large_leg = Self::into_integer(p * (p - 2.0 * x) / (2.0 * (p - x)))?;
+        let y = large_leg as f64;
+        let hypothenuse = Self::into_integer((x.powi(2) + y.powi(2)).sqrt())?;
+        if small_leg + large_leg + hypothenuse == perimeter {
+            return Some(Self {
+                small_leg,
+                large_leg,
+                hypothenuse,
+            });
+        }
+        None
     }
 
-    fn get_large_leg(small_leg: u32, perimeter: u32) -> Option<u32> {
-        todo!()
+    fn into_integer(value: f64) -> Option<u32> {
+        if (value.round() - value).abs() < TOLERANCE && value <= u32::MAX as f64 && value > 0.0 {
+            return Some(value as u32);
+        }
+        None
     }
 
-    fn find_integer_hypotenuse(small_leg: u32, large_leg: u32) -> Option<u32> {
-        todo!()
-    }
-
-    pub fn to_slice(self) -> [u32; 3] {
+    pub fn to_slice(&self) -> [u32; 3] {
         [self.small_leg, self.large_leg, self.hypothenuse]
     }
 }
