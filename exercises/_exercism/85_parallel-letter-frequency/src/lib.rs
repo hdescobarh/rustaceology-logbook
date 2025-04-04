a1d682b3313de18c460987f11dfac5106b4a3155
Hans Escobar <hans-escobar@proton.me>
2025-04-04 14:33:35 -0500

    test: add test get_breakpoints for empty, ascii and non-ascii texts

--- exercises/_exercism/85_parallel-letter-frequency/src/lib.rs
+++ exercises/_exercism/85_parallel-letter-frequency/src/lib.rs
@@ -32,16 +32,70 @@ mod test {
     use crate::*;
 
     #[test]
-    fn ascii_non_empty_text() {
+    fn empty_text() {
+        let text = "";
+        let expect = [0];
+        let cases = [1, 4, 10];
+        for worker_count in cases {
+            assert_eq!(get_breakpoints(text, worker_count), expect)
+        }
+    }
+
+    #[test]
+    fn ascii_text_length_one() {
+        let text = "a";
+        let expect = [0, 1];
+        let cases = [1, 4, 10];
+        for worker_count in cases {
+            assert_eq!(get_breakpoints(text, worker_count), expect)
+        }
+    }
+
+    #[test]
+    fn ascii_text_large_than_one() {
         let text = "ab cd ef gh ";
         let cases = [
-            (1, vec![0, 11]),
-            (4, vec![0, 3, 6, 9, 11]),
-            (6, vec![0, 2, 4, 6, 8, 10, 11]),
-            (10, (0..10).chain(11..12).collect()),
-            (11, (0..12).collect()),
-            (12, (0..12).collect()),
-            (15, (0..12).collect()),
+            (1, vec![0, 12]),
+            (4, vec![0, 3, 6, 9, 12]),
+            (6, vec![0, 2, 4, 6, 8, 10, 12]),
+            (10, (0..11).chain(12..13).collect()),
+            (11, (0..13).collect()),
+            (12, (0..13).collect()),
+            (15, (0..13).collect()),
+        ];
+        for (worker_count, expect) in cases {
+            assert_eq!(get_breakpoints(text, worker_count), expect)
+        }
+    }
+
+    #[test]
+    fn unicode_text_length_one() {
+        let cases = [
+            ("A", [0, 1]),  // one byte,
+            ("à", [0, 2]),  // two bytes
+            ("は", [0, 3]), //three bytes
+            ("👾", [0, 4]), //four bytes
+        ];
+        for worker_count in [1, 2, 3, 4, 7] {
+            for (text, expect) in cases {
+                assert_eq!(get_breakpoints(text, worker_count), expect)
+            }
+        }
+    }
+
+    #[test]
+    fn unicode_text_large_than_one() {
+        let text = "áàは갎智ghЁ"; // 17 bytes, 8 chars at (0,2,4,7,10,13,14,15)
+        let cases = [
+            (1, vec![0, 17]),
+            (2, vec![0, 10, 17]),
+            (3, vec![0, 7, 13, 17]),
+            (7, vec![0, 2, 4, 7, 10, 13, 15, 17]),
+            (8, vec![0, 2, 4, 7, 10, 13, 15, 17]),
+            (9, vec![0, 2, 4, 7, 10, 13, 14, 15, 17]),
+            (15, vec![0, 2, 4, 7, 10, 13, 14, 15, 17]),
+            (17, vec![0, 2, 4, 7, 10, 13, 14, 15, 17]),
+            (20, vec![0, 2, 4, 7, 10, 13, 14, 15, 17]),
         ];
         for (worker_count, expect) in cases {
             assert_eq!(get_breakpoints(text, worker_count), expect)
