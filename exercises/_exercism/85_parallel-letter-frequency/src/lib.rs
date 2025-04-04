8bb47efe2429bf6fe9f3dd8873b21c2e4c4bf44e
Hans Escobar <hans-escobar@proton.me>
2025-04-04 07:50:50 -0500

    feat: add generation of breakpoints to separate the text in slices

--- exercises/_exercism/85_parallel-letter-frequency/src/lib.rs
+++ exercises/_exercism/85_parallel-letter-frequency/src/lib.rs
@@ -9,3 +9,19 @@ pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
         }
     );
 }
+
+fn get_breakpoints(text: &str, worker_count: usize) -> Vec<usize> {
+    let chunk_size = text.len().div_ceil(worker_count);
+    let mut breakpoints: Vec<usize> = vec![0];
+    let mut current = 0;
+    for _ in 1..worker_count {
+        while !text.is_char_boundary(current) {
+            current += 1;
+        }
+        breakpoints.push(current);
+        current += chunk_size;
+        continue;
+    }
+    breakpoints.push(text.len() - 1);
+    breakpoints
+}
