bf0ad37481b95b57f6be0278b96d58b6ae77a450
Hans Escobar <hans-escobar@proton.me>
2025-04-04 08:50:23 -0500

    fix: add limits to the number of chunks and their minimum size

--- exercises/_exercism/85_parallel-letter-frequency/src/lib.rs
+++ exercises/_exercism/85_parallel-letter-frequency/src/lib.rs
@@ -12,17 +12,18 @@ pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
 
 fn get_breakpoints(text: &str, worker_count: usize) -> Vec<usize> {
     let chunk_size = text.len().div_ceil(worker_count);
-    let mut breakpoints: Vec<usize> = vec![0];
+    let mut breakpoints: Vec<usize> = Vec::with_capacity(worker_count.min(text.len()));
     let mut current = 0;
-    for _ in 1..worker_count {
-        while !text.is_char_boundary(current) {
+    for _ in 0..breakpoints.capacity() {
+        while !text.is_char_boundary(current) && current < text.len() - 1 {
             current += 1;
         }
         breakpoints.push(current);
         current += chunk_size;
-        continue;
     }
-    breakpoints.push(text.len() - 1);
+    if let Some(true) = breakpoints.last().map(|v| *v != text.len() - 1) {
+        breakpoints.push(text.len() - 1);
+    }
     breakpoints
 }
 
