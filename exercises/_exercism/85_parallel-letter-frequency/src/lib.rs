87b71b1f1cd7ef32be2dd65e19a28f7e9ba8ccc2
Hans Escobar <hans-escobar@proton.me>
2025-04-08 11:09:36 -0500

    try to optimize runtime

--- exercises/_exercism/85_parallel-letter-frequency/src/lib.rs
+++ exercises/_exercism/85_parallel-letter-frequency/src/lib.rs
@@ -1,19 +1,26 @@
 use std::collections::HashMap;
 use std::thread;
 
+const PARALLEL_THRESHOLD: usize = 50;
+
 pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
-    match input.len() {
-        0 => return HashMap::new(),
-        v if v < 100 => return count(input),
+    match (input.len(), worker_count) {
+        (_, 0) => panic!("worker_count must be greater than zero."),
+        (0, _) => return HashMap::new(),
+        (size, _) if size < PARALLEL_THRESHOLD => return letter_counter(input),
         _ => (),
-    };
+    }
+
     thread::scope(|scope| {
-        let mut result = HashMap::new();
-        let mut handles = vec![];
+        let mut handles = Vec::with_capacity(worker_count);
         for chunk in input.chunks(input.len().div_ceil(worker_count)) {
-            let handle = scope.spawn(move || count(chunk));
+            let handle = scope.spawn(move || letter_counter(chunk));
             handles.push(handle);
         }
+        let mut result = match handles.pop() {
+            Some(handle) => handle.join().unwrap(),
+            None => HashMap::new(),
+        };
         for handle in handles {
             for (letter, count) in handle.join().unwrap() {
                 *result.entry(letter).or_default() += count;
@@ -22,7 +29,7 @@ pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
         result
     })
 }
-fn count(text: &[&str]) -> HashMap<char, usize> {
+fn letter_counter(text: &[&str]) -> HashMap<char, usize> {
     let mut result = HashMap::new();
     for letter in text.iter().flat_map(|line| line.chars()) {
         if !letter.is_alphabetic() {
