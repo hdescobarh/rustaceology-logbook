fd9a034016eed40da362872df496ec15db032a43
Hans Escobar <hans-escobar@proton.me>
2025-04-15 14:47:43 -0500

    feat: add a function to calculate the number of overlapping segments in different rows)

--- exercises/_exercism/87_rectangles/src/lib.rs
+++ exercises/_exercism/87_rectangles/src/lib.rs
@@ -1,5 +1,26 @@
+use std::collections::HashMap;
+const EDGE: u8 = b'+';
+type segment = (usize, usize);
+
 pub fn count(lines: &[&str]) -> u32 {
     todo!(
         "\nDetermine the count of rectangles in the ASCII diagram represented by the following lines:\n{lines:#?}\n."
     );
+}
+
+fn count_segment_overlaps(lines: &[&str]) -> HashMap<segment, usize> {
+    let mut result: HashMap<segment, usize> = HashMap::new();
+    for line in lines {
+        let col_coordinates: Vec<usize> = line
+            .bytes()
+            .enumerate()
+            .filter_map(|(col, byte)| if byte == EDGE { Some(col) } else { None })
+            .collect();
+        for start in 0..col_coordinates.len() - 1 {
+            for end in start + 1..col_coordinates.len() {
+                *result.entry((start, end)).or_default() += 1;
+            }
+        }
+    }
+    result
 }
