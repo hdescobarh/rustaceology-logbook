1b717204337bdaf6b585835b922b823b5e9748e0
Hans Escobar <hans-escobar@proton.me>
2025-04-15 17:04:05 -0500

    feat: implement preliminar solution strategy

--- exercises/_exercism/87_rectangles/src/lib.rs
+++ exercises/_exercism/87_rectangles/src/lib.rs
@@ -1,24 +1,30 @@
 use std::collections::HashMap;
 const EDGE: u8 = b'+';
-type segment = (usize, usize);
+type Segment = (usize, usize);
 
 pub fn count(lines: &[&str]) -> u32 {
-    todo!(
-        "\nDetermine the count of rectangles in the ASCII diagram represented by the following lines:\n{lines:#?}\n."
-    );
+    count_segment_overlaps(lines)
+        .into_values()
+        .map(|count| count * (count - 1) / 2)
+        .sum()
 }
 
-fn count_segment_overlaps(lines: &[&str]) -> HashMap<segment, usize> {
-    let mut result: HashMap<segment, usize> = HashMap::new();
+fn count_segment_overlaps(lines: &[&str]) -> HashMap<Segment, u32> {
+    let mut result: HashMap<Segment, u32> = HashMap::new();
     for line in lines {
         let col_coordinates: Vec<usize> = line
             .bytes()
             .enumerate()
             .filter_map(|(col, byte)| if byte == EDGE { Some(col) } else { None })
             .collect();
+        if col_coordinates.len() < 2 {
+            continue;
+        }
         for start in 0..col_coordinates.len() - 1 {
             for end in start + 1..col_coordinates.len() {
-                *result.entry((start, end)).or_default() += 1;
+                *result
+                    .entry((col_coordinates[start], col_coordinates[end]))
+                    .or_default() += 1;
             }
         }
     }
