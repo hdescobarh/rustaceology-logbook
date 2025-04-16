910cb79c5c1ae3ebe9d298ac52158f5c260231bf
Hans Escobar <hans-escobar@proton.me>
2025-04-15 19:34:52 -0500

    refactor: implement solution. All test passed

--- exercises/_exercism/87_rectangles/src/lib.rs
+++ exercises/_exercism/87_rectangles/src/lib.rs
@@ -1,32 +1,84 @@
-use std::collections::HashMap;
-const EDGE: u8 = b'+';
-type Segment = (usize, usize);
+use std::collections::{HashMap, HashSet};
+const VERTEX: u8 = b'+';
+const H_LINE: u8 = b'-';
+const V_LINE: u8 = b'|';
+const EMPTY: u8 = b' ';
 
 pub fn count(lines: &[&str]) -> u32 {
-    count_segment_overlaps(lines)
-        .into_values()
-        .map(|count| count * (count - 1) / 2)
-        .sum()
+    ParsedInput::new(lines)
+        .map(|parsed| parsed.count_rectangles())
+        .unwrap_or_default()
 }
 
-fn count_segment_overlaps(lines: &[&str]) -> HashMap<Segment, u32> {
-    let mut result: HashMap<Segment, u32> = HashMap::new();
-    for line in lines {
-        let col_coordinates: Vec<usize> = line
-            .bytes()
-            .enumerate()
-            .filter_map(|(col, byte)| if byte == EDGE { Some(col) } else { None })
-            .collect();
-        if col_coordinates.len() < 2 {
-            continue;
-        }
-        for start in 0..col_coordinates.len() - 1 {
-            for end in start + 1..col_coordinates.len() {
-                *result
-                    .entry((col_coordinates[start], col_coordinates[end]))
-                    .or_default() += 1;
+struct ParsedInput {
+    /// Maps proper horizontal edges (start_col, end_col) to the rows where they are
+    hor_edges_to_rows: HashMap<(usize, usize), Vec<usize>>,
+    /// Contains the coordinates of every vertical lines
+    ver_edges_coordinates: HashSet<(usize, usize)>,
+}
+
+impl ParsedInput {
+    fn new(lines: &[&str]) -> Option<Self> {
+        lines.first()?;
+        let mut hor_edges = HashMap::new();
+        let mut ver_edges = HashSet::new();
+        for (row, line) in lines.iter().enumerate() {
+            let mut segment_start: Option<Vec<usize>> = None;
+            for (col, value) in line.bytes().enumerate() {
+                match value {
+                    VERTEX if segment_start.is_none() => {
+                        segment_start = Some(vec![col]);
+                        ver_edges.insert((row, col));
+                    }
+                    VERTEX => {
+                        for start in segment_start.as_ref().unwrap() {
+                            hor_edges
+                                .entry((*start, col))
+                                .and_modify(|v: &mut Vec<usize>| v.push(row))
+                                .or_insert(vec![row]);
+                        }
+                        ver_edges.insert((row, col));
+                        segment_start.as_mut().unwrap().push(col);
+                    }
+                    EMPTY => segment_start = None,
+                    V_LINE => {
+                        segment_start = None;
+                        ver_edges.insert((row, col));
+                    }
+                    H_LINE => continue,
+                    _ => return None,
+                }
             }
         }
+        Some(Self {
+            hor_edges_to_rows: hor_edges,
+            ver_edges_coordinates: ver_edges,
+        })
     }
-    result
+
+    /// generate all the possible pair of rows
+    fn permutate_rows(rows: &[usize]) -> Vec<(usize, usize)> {
+        let mut result: Vec<(usize, usize)> = Vec::with_capacity(rows.len() * (rows.len() - 1) / 2);
+        for i in 0..rows.len() - 1 {
+            for i_prime in i + 1..rows.len() {
+                result.push((rows[i], rows[i_prime]))
+            }
+        }
+        result
+    }
+
+    fn count_rectangles(&self) -> u32 {
+        let mut counter = 0;
+        for (hor_segment, rows) in &self.hor_edges_to_rows {
+            for candidate in Self::permutate_rows(rows) {
+                if (candidate.0 + 1..candidate.1).all(|row| {
+                    self.ver_edges_coordinates.contains(&(row, hor_segment.0))
+                        && self.ver_edges_coordinates.contains(&(row, hor_segment.1))
+                }) {
+                    counter += 1;
+                };
+            }
+        }
+        counter
+    }
 }
