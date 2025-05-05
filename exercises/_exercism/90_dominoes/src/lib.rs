1dff79e50b51eb2e2309bb725c30c5bbd380b45e
Hans Escobar <hans-escobar@proton.me>
2025-05-05 09:07:32 -0500

    feat: add comment with problem modeling

--- exercises/_exercism/90_dominoes/src/lib.rs
+++ exercises/_exercism/90_dominoes/src/lib.rs
@@ -1,3 +1,9 @@
+use std::collections::HashSet;
+
+// The problem can be modeled as finding a Hamiltonian cycle in an
+// undirected graph G, where each domino represents a node, and an edge
+// connects two nodes if the corresponding stones have matching halves.
+// In other words, this is a case of the Traveling Salesman Problem.
 pub fn chain(input: &[(u8, u8)]) -> Option<Vec<(u8, u8)>> {
     todo!(
         "From the given input '{input:?}' construct a proper dominoes chain or return None if it is not possible."
