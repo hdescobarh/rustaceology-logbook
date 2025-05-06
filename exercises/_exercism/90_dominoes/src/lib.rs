06c5bbd5ec1a1c95244182179565aba66f4eafcd
Hans Escobar <hans-escobar@proton.me>
2025-05-06 02:19:28 -0500

    refactor: add backtracking values

--- exercises/_exercism/90_dominoes/src/lib.rs
+++ exercises/_exercism/90_dominoes/src/lib.rs
@@ -15,9 +15,10 @@ pub fn chain(input: &[(u8, u8)]) -> Option<Vec<(u8, u8)>> {
     }
     let partition = &input[1..];
     let power_set = PowerSet::from_cardinality(partition.len());
-    let mut cost: HashMap<(usize, usize), f64> = HashMap::with_capacity(2 * (1 << partition.len()));
+    let mut cost_and_precursors: HashMap<(usize, usize), (f64, (usize, usize))> =
+        HashMap::with_capacity(2 * (1 << partition.len()));
     for (subset_k, node_i) in power_set.iter_singletons() {
-        cost.insert((node_i, subset_k), edge_weight(0, node_i, input));
+        cost_and_precursors.insert((node_i, subset_k), (edge_weight(0, node_i, input), (0, 0)));
     }
 
     for (subset_k, elements) in power_set.iter_non_singletons() {
@@ -25,20 +26,21 @@ pub fn chain(input: &[(u8, u8)]) -> Option<Vec<(u8, u8)>> {
             let mut min_cost = f64::INFINITY;
             let subset_k_excluding_node_i = subset_k & !(1 << node_i);
             for &node_j in &power_set.subset_elements[&subset_k_excluding_node_i] {
-                let current_cost =
-                    cost[&(node_j, subset_k_excluding_node_i)] + edge_weight(node_i, node_j, input);
+                let current_cost = cost_and_precursors[&(node_j, subset_k_excluding_node_i)].0
+                    + edge_weight(node_i, node_j, input);
                 if min_cost > current_cost {
                     min_cost = current_cost;
                 }
             }
-            cost.insert((node_i, subset_k), min_cost);
+            cost_and_precursors.insert((node_i, subset_k), (min_cost, (0, 0)));
         }
     }
 
     let mut min_cost = f64::INFINITY;
     let subset_k = *power_set.size_to_subsets.last().unwrap().last().unwrap();
     for &node_i in &power_set.subset_elements[&subset_k] {
-        let current_cost = cost[&(node_i, subset_k)] + edge_weight(0, node_i, input);
+        let current_cost =
+            cost_and_precursors[&(node_i, subset_k)].0 + edge_weight(0, node_i, input);
         if min_cost > current_cost {
             min_cost = current_cost;
         }
