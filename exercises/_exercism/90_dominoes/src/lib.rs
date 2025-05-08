e50866a6fbf6ddaa9e676d661a18837244807581
Hans Escobar <hans-escobar@proton.me>
2025-05-08 11:35:57 -0500

    feat: add PsedoMultiGraph and methods to create it and manipulate its edges

--- exercises/_exercism/90_dominoes/src/lib.rs
+++ exercises/_exercism/90_dominoes/src/lib.rs
@@ -1,5 +1,35 @@
+use std::collections::HashMap;
+
+/// The problem can be modeled as finding and Eulerian cycle where each stone is an edge
 pub fn chain(input: &[(u8, u8)]) -> Option<Vec<(u8, u8)>> {
     todo!(
         "From the given input '{input:?}' construct a proper dominoes chain or return None if it is not possible."
     );
+}
+
+/// A symmetric graph-like structure which allows self-edges and multiple edges between a pair of nodes
+struct PseudoMultiGraph {
+    /// a map node_i ↦ (node_j, multiplicity_j)
+    adjacency: HashMap<u8, HashMap<u8, usize>>,
+}
+
+impl PseudoMultiGraph {
+    fn from_edges(input: &[(u8, u8)]) -> Self {
+        let mut adjacency: HashMap<u8, HashMap<u8, usize>> = HashMap::new();
+        for (u, v) in input.iter().flat_map(|(u, v)| [(u, v), (v, u)]) {
+            *adjacency.entry(*u).or_default().entry(*v).or_default() += 1
+        }
+        Self { adjacency }
+    }
+
+    fn remove_edge(&mut self, node1: u8, node2: u8) {
+        for (u, v) in [(node1, node2), (node2, node1)] {
+            let multiset = self.adjacency.get_mut(&u).unwrap();
+            if multiset[&v] == 1 {
+                multiset.remove_entry(&v);
+            } else {
+                *multiset.get_mut(&v).unwrap() -= 1;
+            }
+        }
+    }
 }
